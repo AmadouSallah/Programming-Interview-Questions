769bb10826ad286be5fab2c692ad3a1075372d5f
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-04-24 18:26:06 -0700

    Added problem statement

---
+++ cracking_the_coding_interview_questions/chp_1/problem_1.3_a.rb
@@ -0,0 +1,5 @@
+# This is problem  1.3 from the 4th edition:
+
+# Design an algorithm and write code to remove the duplicate characters in a string without using any additional buffer
+# NOTE: One or two additional variables are fine An extra copy of the array is not
+
