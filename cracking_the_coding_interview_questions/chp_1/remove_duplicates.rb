129b4a049e20a0f0f356e5ced301f4016bc004e1
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-04-25 18:33:14 -0700

    Renamed remove_dublicates.rb to remove_duplicates.rb

--- cracking_the_coding_interview_questions/chp_1/remove_duplicates.rb
+++ cracking_the_coding_interview_questions/chp_1/remove_duplicates.rb
@@ -4,6 +4,7 @@
 # NOTE: One or two additional variables are fine An extra copy of the array is not
 
 # PSEUDOCODE
+# 0) We return the input string if its lenth is less than 2 (no duplicates in this case)
 # 1) We define a varialbe "tail" to represent the boundary to the left of which there is no duplicates (initially at position 1)
 # 2) We make an outer loop going from position 1 to the end of the input string.
 # a) For each i of the outer loop, we start an inner loop whose counter j goes from 0 to tail-1 at most. If we find a duplicate to string[i], we break from this loop.
@@ -37,6 +38,22 @@ def remove_duplicates(string)
   string.slice(0, tail)
 end
 
+# SOLUTION 2: Using additional memory
+# PSEUDOCODE:
+# 0) We return the string if string.lenth is less than 2 (no duplicates in this case)
+# 1) We initialize an empty array, no_duplicates
+# 2) We iterate through all the characters of the input string, and we put each of them in the no_duplicates array as long as the array doesn't contain the character.
+# 3) Once we are done looping through the input string, we join the no_duplicates array and return the joined string.
+
+def remove_duplicates_2(string)
+  return string if string.length < 2
+  no_duplicates = []
+  string.chars.each do |letter|
+    no_duplicates << letter unless no_duplicates.include?(letter)
+  end
+  no_duplicates.join()
+end
+
 # TEST Cases
 p remove_duplicates("") == ""
 p remove_duplicates(" ") == " "
@@ -45,3 +62,11 @@ p remove_duplicates("aaa") == "a"
 p remove_duplicates("abababa") == "ab"
 p remove_duplicates("aaabbb") == "ab"
 p remove_duplicates("abcd") == "abcd"
+
+p remove_duplicates_2("") == ""
+p remove_duplicates_2(" ") == " "
+p remove_duplicates_2("a") == "a"
+p remove_duplicates_2("aaa") == "a"
+p remove_duplicates_2("abababa") == "ab"
+p remove_duplicates_2("aaabbb") == "ab"
+p remove_duplicates_2("abcd") == "abcd"
