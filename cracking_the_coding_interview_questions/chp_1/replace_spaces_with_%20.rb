1a4fedd0faacbe0695c3eedd365f7dc88434d0ca
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-04-26 15:19:36 -0700

    Updated replace_spaces_with_%20.rb

--- cracking_the_coding_interview_questions/chp_1/replace_spaces_with_%20.rb
+++ cracking_the_coding_interview_questions/chp_1/replace_spaces_with_%20.rb
@@ -1,27 +1,27 @@
 # Problem 1.4, 5th ed.
 # Write a method to replace all spaces in a string with '%20'. You may assume that the string has sufficient space at the end of the string to hold the additional characters and that you are given the "true"  lenth of the string.
 
-# Example Input : "Hi my   friend " Output: "Hi%20my%20friend"
+# Example Input : "Hi my   friend " Output: "Hi%20%my%20%20%20friend"
 
 # Solution 1
 # Pseudocode
-# We will call ruby's regular expression gsub to replace every space by '20%'
+# We will call ruby's regular expression gsub to replace every space by '%20'
 
 def replace_spaces(string, length)
-  string.gsub(/\s/) { '20%' }
+  string.gsub(/\s/) { '%20' }
 end
 
 # Solution 2
 # Pseudocode
 # We initialize an empty string, string_without_spaces.
-# We go through the input string; If we encounter an empty space, we add "20%" to string_without_spaces, otherwise we add the input string character to it.
+# We go through the input string; If we encounter an empty space, we add "%20" to string_without_spaces, otherwise we add the input string character to it.
 # When we are done looping through the input string, we return string_without_spaces
 
 def replace_spaces_2(string, length)
   string_without_spaces = ""
   string.chars.each do |letter|
     if letter == " "
-      string_without_spaces << "20%"
+      string_without_spaces << "%20"
     else
       string_without_spaces << letter
     end
@@ -30,9 +30,11 @@ def replace_spaces_2(string, length)
 end
 
 p replace_spaces('', 0) == ""
-p replace_spaces('  ', 2) == "20%20%"
-p replace_spaces("I love    R U B Y ", 18) == "I20%love20%20%20%20%R20%U20%B20%Y20%"
+p replace_spaces('  ', 2) == "%20%20"
+p replace_spaces("I love    R U B Y ", 18) == "I%20love%20%20%20%20R%20U%20B%20Y%20"
+p replace_spaces("Hi", 2) == "Hi"
 
 p replace_spaces_2('', 0) == ""
-p replace_spaces_2('  ', 2) == "20%20%"
-p replace_spaces_2("I love    R U B Y ", 18) == "I20%love20%20%20%20%R20%U20%B20%Y20%"
+p replace_spaces_2('  ', 2) == "%20%20"
+p replace_spaces_2("I love    R U B Y ", 18) == "I%20love%20%20%20%20R%20U%20B%20Y%20"
+p replace_spaces_2("Hi", 2) == "Hi"
