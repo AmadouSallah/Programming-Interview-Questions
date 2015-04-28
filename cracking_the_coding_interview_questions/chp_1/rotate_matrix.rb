ff7a358bcd7889db66e5837c2d54197f4e11be44
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-04-28 02:26:27 -0700

    Added rotate_matrix.rb file with input/output example

--- cracking_the_coding_interview_questions/chp_1/rotate_matrix.rb
+++ cracking_the_coding_interview_questions/chp_1/rotate_matrix.rb
@@ -2,13 +2,7 @@
 # Given an image represented by an NxN matrix, where each pixel in the image is 4 bytes,
 # write a method to rotate the image by 90 degrees. Can you do this in place?
 
-# Example:
-#     ABCD
-#     EFGH
-#     IJKL
-#     MNOP
-# becomes
-#     MIEA
-#     NFGB
-#     OJKC
-#     PLHD
+# [1][2][3][4]             [3][9][5][1]
+# [5][6][7][8]  BECOMES    [4][0][6][2]
+# [9][0][1][2]             [5][1][7][3]
+# [3][4][5][6]             [6][2][8][4]
