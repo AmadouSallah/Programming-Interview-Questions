09252be91bc772640453dcca6e591aca66ec73de
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-04-28 02:46:54 -0700

    Added first solution to rotate_matrix.rb

--- cracking_the_coding_interview_questions/chp_1/rotate_matrix.rb
+++ cracking_the_coding_interview_questions/chp_1/rotate_matrix.rb
@@ -6,3 +6,22 @@
 # [5][6][7][8]  BECOMES    [4][0][6][2]
 # [9][0][1][2]             [5][1][7][3]
 # [3][4][5][6]             [6][2][8][4]
+
+# SOLUTION 1
+# PSEUDOCODE:
+# We create an empty array, rotated_by_90, that will contain the result of rotating the input matrix by 90 degrees
+# We transpose the input matrix, and for each row of the transposed matrix, we reverse it and append it to rotated_by_90
+# We return rotated_by_90
+
+def rotate(matrix)
+  rotated_by_90 = []
+  matrix.transpose.each {|row| rotated_by_90 << row.reverse}
+  rotated_by_90
+end
+
+array = [[1, 2, 3, 4], [5, 6, 7, 8],[9, 0, 1, 2],[3, 4, 5, 6]]
+
+p rotate([]) == []
+p rotate([[1]]) == [[1]]
+p rotate([[1, 2]]) == [[1], [2]]
+p rotate(array) == [[3, 9, 5, 1], [4, 0, 6, 2], [5, 1, 7, 3], [6, 2, 8, 4]]
