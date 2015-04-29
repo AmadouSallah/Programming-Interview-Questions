6e5e8ef58c71721f8c44bcb9f5d9df1aa06ddf57
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-04-28 23:51:24 -0700

    Added pivot_index.rb file

---
+++ pivot_index.rb
@@ -0,0 +1,4 @@
+# Problem: Write a method that returns the "pivot" index of a list of integers.
+# We define the pivot index as the index where the sum of the numbers on the left is equal to the sum of the numbers on the right.
+# Example: Given [1, 4, 6, 3, 2], the method should return 2, since the sum of the numbers to the left of index 2 is equal to the sum of numbers to the right of index 2 (1 + 4 = 3 + 2).
+# If no such index exists, it should return -1. If there are multiple pivots, you can return the left-most pivot.
