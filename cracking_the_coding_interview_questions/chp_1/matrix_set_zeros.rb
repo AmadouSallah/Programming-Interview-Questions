e326b959021a018aec893ffcc440d690e35579af
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-04-28 19:29:23 -0700

    Added matrix_set_zeros.rb file

---
+++ cracking_the_coding_interview_questions/chp_1/matrix_set_zeros.rb
@@ -0,0 +1,16 @@
+# Problem 1.7
+#1.7 Write an algorithm such that if an element in an MxN matrix is 0, its entire row and column is set to 0
+# Example:
+# 1 0 1 1 0
+# 0 1 1 1 0
+# 1 1 1 1 1
+# 1 0 1 1 1
+# 1 1 1 1 1
+# would results into:
+# 0 0 0 0 0
+# 0 0 0 0 0
+# 0 0 1 1 0
+# 0 0 0 0 0
+# 0 0 1 1 0
+
+# PSEUDOCODE
