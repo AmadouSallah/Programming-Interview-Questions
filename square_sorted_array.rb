bd074bf3315335c21bb7bcee246fc9a601379b4a
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-05-02 12:25:45 -0700

    Added first solution to square_sorted_array.rb

--- square_sorted_array.rb
+++ square_sorted_array.rb
@@ -1,4 +1,18 @@
 # Problem:
 # Given a sorted array, how would you square each element of this array, while keeping it sorted?
 # Example array = [-8, -3, -1, 0, 1, 4, 5, 9] => [0, 1, 1, 9, 16, 25, 64, 81]
-#Pseudocode:
+
+# Solution 1
+# Pseudocode: We square each element of the array, we then sort the array and return it.
+
+def square_sorted_array(array)
+  squared_array = []
+  array.each {|element| squared_array << element * element}
+  squared_array.sort
+end
+
+# TEST DRIVE:
+array = [-8, -3, -1, 0, 1, 4, 5, 9]
+p square_sorted_array([]) == []
+p square_sorted_array([-2]) == [4]
+p square_sorted_array(array) == [0, 1, 1, 9, 16, 25, 64, 81]
