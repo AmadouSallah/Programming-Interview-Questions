52201a2182b3ca9bcdc76027c7b242a9006a9fe9
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-05-05 16:32:49 -0700

    Added are_anagrams.rb file

---
+++ are_anagrams.rb
@@ -0,0 +1,5 @@
+# Write the method are_anagrams? that takes two strings as arguments and returns true if they are anagrams of each other or false if they are not.
+
+# Explain the time and space complexity of your algorithm.
+
+# Bonus: Can you do it without using the Ruby method #sort?
