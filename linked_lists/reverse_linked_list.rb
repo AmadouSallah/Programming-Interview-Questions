bed7a511b97535c87450d64297fcb3190cb08d88
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-05-11 11:43:42 -0700

    Added Pseudocode to iterative solution

--- linked_lists/reverse_linked_list.rb
+++ linked_lists/reverse_linked_list.rb
@@ -1,6 +1,17 @@
 require_relative 'implement_linked_list'
 
 # Iterative Solution
+# PSEUDOCODE:
+# 1) If the head node is nil, we just return it
+# 2) We initialize a temporal_node to nil
+# 3) We loop through the linked list starting from the head node (the input) until the last node. We will be moving head 1 node at a time until we reach the last node
+# a) We define a variable next_node and have it point to the next node of head
+# b) We make the next of head point to the temporal_node
+# c) We set temporal_node equal to head
+# d) We set head equal to next_node
+# e) We repeat a) to d) until head becomes nil
+# 4) We then return temporal_node
+
 def reverse(head)
   return head if head == nil
 
@@ -13,6 +24,7 @@ def reverse(head)
   end
   temporal_node
 end
+
 # TEST DRIVE
 e = Node.new(5, nil)
 d = Node.new(4, e)
