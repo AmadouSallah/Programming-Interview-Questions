b114b1ad5aa329d16e233566211870985c05e739
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-05-11 17:52:31 -0700

    Added problem statement

---
+++ linked_lists/insert_in_sorted_list.rb
@@ -0,0 +1,3 @@
+# Problem: Given a sorted linked list (assume sorted in ascending way), write a function to insert a node of a given value
+# in a sorted way, that is, the linked list should be sorted after the insertion
+
