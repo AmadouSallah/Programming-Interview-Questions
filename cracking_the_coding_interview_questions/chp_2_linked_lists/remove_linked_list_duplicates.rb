73687057556c7de11c2fbce198d43da575c5f142
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-05-20 14:40:58 -0700

    Added pseudocode to 1st solution of remove_linked_list_duplicates.rb

--- cracking_the_coding_interview_questions/chp_2_linked_lists/remove_linked_list_duplicates.rb
+++ cracking_the_coding_interview_questions/chp_2_linked_lists/remove_linked_list_duplicates.rb
@@ -5,6 +5,13 @@ require_relative './../../linked_lists/implement_linked_list.rb'
 # FOLLOW UP
 # How would you solve this problem if a temporary buffer is not allowed?
 
+# SOLUTION 1
+# Pseudocode
+# We intially define 2 pointers,current_node and previous_node, that point to the head of the linked list
+# We also initialize a hash, number_of_repeats, with a default value of 0
+# We loop through the linked list and we add the values of the nodes in the hash. The hash values will represent the number of times each node value is present in list.
+# If we reach a node that is not a repeat, we increment the hash key value of the corresponding node value by 1; we then set the next node of 'previous_node' to the 'current_node'
+# If we reach a node whose value is a repeat (the corresponding hash value is > 0), we set the next node of 'previous_node' to the next node of the 'current_node'
 def remove_linked_list_duplicates(head)
   current_node = head
   previous_node = head
@@ -20,6 +27,8 @@ def remove_linked_list_duplicates(head)
   end
   head
 end
+
+# SOLUTION 2: without a temporary buffer
 
 # TEST DRIVE
 e = Node.new(1, nil)
