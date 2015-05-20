388d10b1dc37f914b60158de6fb4728d6d95676f
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-05-20 12:11:39 -0700

    Added problem statement

---
+++ cracking_the_coding_interview_questions/chp_2_linked_lists/remove_linked_list_duplicates.rb
@@ -0,0 +1,4 @@
+# Problem 2.1 (Cracking the Coding Interview):
+# Write code to remove duplicates from an unsorted linked list
+# FOLLOW UP
+# How would you solve this problem if a temporary buffer is not allowed?
