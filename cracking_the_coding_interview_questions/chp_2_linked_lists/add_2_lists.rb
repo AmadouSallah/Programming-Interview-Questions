c6c2a842c1d4f1275a278aa192c3b7e7cbd6efd0
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-05-24 20:53:53 -0700

    Added problem statement for add_2_lists.rb

---
+++ cracking_the_coding_interview_questions/chp_2_linked_lists/add_2_lists.rb
@@ -0,0 +1,5 @@
+# Problem: You have two numbers represented by a linked list, where each node contains a single digit.
+# The digits are stored in reverse order, such that the 1’s digit is at the head of the list.
+# Write a function that adds the two numbers and returns the sum as a linked list.
+# FOLLOW UP
+# Support the digits are stored in forward order. Repeat the above problem.
