2105a5d4c473d45b34c46e41da5ba6576e674bf1
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-05-25 18:38:22 -0700

    Added 1st solution

--- is_palindrome.rb
+++ is_palindrome.rb
@@ -1,2 +1,21 @@
 # Problem: Write a code to check is a string is a palindrome or not
 # Definition: A palindrome is a word, phrase, or sequence that reads the same backward as forward, e.g., madam.
+
+def is_palindrome?(string)
+  return true if string.length < 2
+  i = 0
+  j = string.length - 1
+  while i <= j
+    return false if string[i] != string[j]
+    i += 1
+    j -= 1
+  end
+  true
+end
+
+# TEST DRIVE
+p is_palindrome?("") == true
+p is_palindrome?("a") == true
+p is_palindrome?("ab") == false
+p is_palindrome?("madam") == true
+p is_palindrome?("nurses run") == false
