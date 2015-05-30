01f510f769ab7b7ccbb361df8193c9a22e09439e
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-05-29 23:32:01 -0700

    Added problem statement

--- cracking_the_coding_interview_questions/chp_2_linked_lists/find_node_at_loop_beginning.rb
+++ cracking_the_coding_interview_questions/chp_2_linked_lists/find_node_at_loop_beginning.rb
@@ -1,1 +1,6 @@
+# Problem: Given a circular linked list, implement an algorithm which returns node at the beginning of the loop
 
+# DEFINITION
+# Circular linked list: A (corrupt) linked list in which a node’s next pointer points to an earlier node, so as to make a loop in the linked list
+# EXAMPLE
+# Input: A -> B -> C -> D -> E -> C [the same C as earlier] Output: C
