51ce2a60599af45b75af7676199fdb698ec69e2e
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-06-01 00:40:32 -0700

    Added problem statement

---
+++ dynamic_programming/longest_common_subsequence.rb
@@ -0,0 +1,3 @@
+# Given two sequences, find the length of the longest subsequence present in both of them.
+# A subsequence is a sequence that appears in the same relative order, but not necessarily contiguous.
+# For example, “abc”, “abg”, “bdf”, “aeg”, ‘”acefg”, .. etc are subsequences of “abcdefg”.
