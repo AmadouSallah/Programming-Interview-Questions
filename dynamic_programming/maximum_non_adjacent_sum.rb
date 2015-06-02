1ef13ffbd04f98f5ab47ad4cf683cd75d1b428a1
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-06-01 23:51:42 -0700

    Added maximum_non_adjacent_sum.rb file

---
+++ dynamic_programming/maximum_non_adjacent_sum.rb
@@ -0,0 +1,3 @@
+# Problem: Given an array of positive numbers, find the maximum sum of a subsequence with the constraint that no 2 numbers in the sequence should be adjacent in the array.
+# Example [3, 2, 7, 10] should return 13 (sum of 3 and 10);  [3, 2, 5, 10, 7] should return 15 (sum of 3, 5 and 7).
+# Answer the question in most efficient way.
