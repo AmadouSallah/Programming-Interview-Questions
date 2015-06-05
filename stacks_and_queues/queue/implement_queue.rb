d8f17217d40e936808d2ccc2134f0573df4797b9
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-06-05 01:08:12 -0700

    Finished implementing queue

--- stacks_and_queues/queue/implement_queue.rb
+++ stacks_and_queues/queue/implement_queue.rb
@@ -3,3 +3,52 @@
 # and deletions are done at another end (front). The first element to be inserted
 # is the first one to be deleted. Hence, it is called First In First Out (FIFO)
 
+class Queue
+  attr_accessor :store
+  def initialize
+    @store = []
+  end
+
+  # Adds an element to the back of the queue
+  def enqueue(element)
+    @store << element
+  end
+
+  # removes the element at the front and returns it
+  def dequeue
+    raise "Queue Underflow - The queue is empty" if self.is_empty?
+    @store.shift
+  end
+
+  # returns the element at the front (without removing it)
+  def peek
+    @store.first
+  end
+
+  # returns the size of the queue
+  def size
+    @store.length
+  end
+
+  # checks to see if the queue is empty or not
+  def is_empty?
+    self.size == 0
+  end
+
+end
+
+# TEST DRIVE
+# my_queue = Queue.new
+
+# puts "\nInitially, the queue is #{my_queue.store} with a size of #{my_queue.size}"
+
+# puts "\nAdding A, B, C, and D to the queue:"
+# my_queue.enqueue('A')
+# my_queue.enqueue('B')
+# my_queue.enqueue('C')
+# my_queue.enqueue('D')
+# puts "The queue is now #{my_queue.store} with a size of #{my_queue.size}."
+
+# puts "\nRemoving the first element:\n"
+# element = my_queue.dequeue
+# puts "Element #{element} has been removed from the queue, which is now: #{my_queue.store}"
