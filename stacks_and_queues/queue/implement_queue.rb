949cd170b7d28b84865fa7a6665d7038e68ae7a8
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-06-05 00:30:39 -0700

    Added queue definition

---
+++ stacks_and_queues/queue/implement_queue.rb
@@ -0,0 +1,5 @@
+# Implementing a queue
+# A queue is an ordered list in which insertions are done at one end (back)
+# and deletions are done at another end (front). The first element to be inserted
+# is the first one to be deleted. Hence, it is called First In First Out (FIFO)
+
