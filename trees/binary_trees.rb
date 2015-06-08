95ba3bb10161c3beef4ec4fee740f309f7a62356
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-06-08 12:11:43 -0700

    Added Node class

---
+++ trees/binary_trees.rb
@@ -0,0 +1,8 @@
+class Node
+  attr_accessor :value, :left_child, :right_child
+  def initialize(value = nil, left_child = nil, right_child = nil)
+    @value = value
+    @left_child = left_child
+    @right_child = right_child
+  end
+end
