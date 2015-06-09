83ae0e8fdbc0dfc59840baeae4ab68b7b27d2475
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-06-08 21:33:44 -0700

    Added solution using BFS

--- trees/find_max.rb
+++ trees/find_max.rb
@@ -1,18 +1,37 @@
 require_relative 'binary_trees'
+require_relative './../stacks_and_queues/queue/implement_queue'
 
 # Problem: Given the root of a binary tree, write a program to find the maximum element present in it.
 
 # SOLUTION 1 Using DFS ( complexity: 0(n) time and 0(n) space)
-def find_max_element(root)
+def find_max_element_DFS(root)
   return if !root
   root_value = root.value
   left_max = root.value
   right_max = root.value
 
-  left_max = find_max_element(root.left_child) if root.left_child
-  right_max = find_max_element(root.right_child) if root.right_child
+  left_max = find_max_element_DFS(root.left_child) if root.left_child
+  right_max = find_max_element_DFS(root.right_child) if root.right_child
 
   maximum(root_value, maximum(left_max, right_max))
+end
+
+# SOLUTION 2: USING BFS (complexity: 0(n) time and 0(n) space)
+def find_max_element_BFS(root)
+  return if !root
+  queue = Queue.new()
+  max_element = root.value
+  queue.enqueue(root)
+
+  while (!queue.is_empty?)
+    temp_node = queue.dequeue
+    max_element = maximum(max_element, temp_node.value)
+
+    queue.enqueue(temp_node.left_child) if temp_node.left_child
+    queue.enqueue(temp_node.right_child) if temp_node.right_child
+
+  end
+  max_element
 end
 
 def maximum(a, b)
@@ -46,4 +65,5 @@ a.right_child = e
 b.left_child = f
 b.right_child = g
 
-p find_max_element(root)
+p find_max_element_DFS(root)
+p find_max_element_BFS(root)
