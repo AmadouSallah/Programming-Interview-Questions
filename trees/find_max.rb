bd64f85b9cad96f8c306bc68eba25fef806e5008
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-06-08 21:17:58 -0700

    Added solution using Depth First Search

--- trees/find_max.rb
+++ trees/find_max.rb
@@ -1,1 +1,49 @@
+require_relative 'binary_trees'
+
 # Problem: Given the root of a binary tree, write a program to find the maximum element present in it.
+
+# SOLUTION 1 Using DFS ( complexity: 0(n) time and 0(n) space)
+def find_max_element(root)
+  return if !root
+  root_value = root.value
+  left_max = root.value
+  right_max = root.value
+
+  left_max = find_max_element(root.left_child) if root.left_child
+  right_max = find_max_element(root.right_child) if root.right_child
+
+  maximum(root_value, maximum(left_max, right_max))
+end
+
+def maximum(a, b)
+  a > b ? a : b
+end
+
+# TEST DRIVE
+#      1
+#    /    \
+#   2      30
+#  / \    / \
+# 4   5  6   -7
+
+binary_tree = Binary_Tree.new()
+root = Node.new(1)
+a = Node.new(2)
+b = Node.new(30)
+d = Node.new(4)
+e = Node.new(5)
+f = Node.new(6)
+g = Node.new(-7)
+
+binary_tree.root = root
+
+binary_tree.root.left_child = a
+binary_tree.root.right_child = b
+
+a.left_child = d
+a.right_child = e
+
+b.left_child = f
+b.right_child = g
+
+p find_max_element(root)
