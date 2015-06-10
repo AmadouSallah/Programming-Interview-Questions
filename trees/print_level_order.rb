a68e25fbd925821597ce6f0ef7aa49bbede22c6b
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-06-09 20:20:35 -0700

    Added problem statement

---
+++ trees/print_level_order.rb
@@ -0,0 +1,13 @@
+# Problem: Given a binary tree, print out the tree in level order (ie, from left to right, level by level). Output a newline after the end of each level
+# For example, for the following binary tree,
+
+#      1
+#    /    \
+#   2      3
+#  / \    / \
+# 4   5  6   7
+
+# the output will be:
+# 1
+# 2 3
+# 4 5 6 7
