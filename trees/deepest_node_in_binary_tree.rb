3c903554f7324728a5f934256c12a4ce5def7c6a
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-06-11 16:52:48 -0700

    Added solution to deepest_node_in_binary_tree.rb

--- trees/deepest_node_in_binary_tree.rb
+++ trees/deepest_node_in_binary_tree.rb
@@ -1,9 +1,20 @@
 require_relative 'binary_trees'
+require_relative './../stacks_and_queues/queue/implement_queue'
 
 # Problem: Write a program that takes a binary tree root node and returns the deepest node
 
 def deepest_node(root)
+  return root if !root
+  queue = Queue.new()
+  node = Node.new()
+  queue.enqueue(root)
+  while !queue.is_empty?
+    node = queue.dequeue
 
+    queue.enqueue(node.left_child) if node.left_child
+    queue.enqueue(node.right_child) if node.right_child
+  end
+  node
 end
 
 # TEST DRIVE
@@ -17,7 +28,7 @@ end
 
 h = Node.new(8)
 g = Node.new(7)
-f = Node.new(6, g)
+f = Node.new(6, h)
 e = Node.new(5)
 d = Node.new(4)
 c = Node.new(3, f, g)
@@ -26,4 +37,4 @@ a = Node.new(1, b, c)
 
 root = a
 
-p deepest_node(root)
+p deepest_node(root) # Should return the node with value 8 (h)
