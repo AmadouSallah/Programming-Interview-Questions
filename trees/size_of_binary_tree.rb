07fac786993b617052f462f1eb8160d2d4b9f1ec
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-06-11 02:45:07 -0700

    Added a BFS solution (level order traversal)

--- trees/size_of_binary_tree.rb
+++ trees/size_of_binary_tree.rb
@@ -9,6 +9,22 @@ def size_of_binary_tree_DFS(root)
   return 1 + size_of_binary_tree_DFS(root.left_child) + size_of_binary_tree_DFS(root.right_child)
 end
 
+# SOLUTION 2: Breath First Search (BFS)
+def size_of_binary_tree_BFS(root)
+  return 0 if !root
+
+  count = 0
+  queue = Queue.new()
+  queue.enqueue(root)
+  while !queue.is_empty?
+    temporal_node = queue.dequeue
+    count += 1
+    queue.enqueue(temporal_node.left_child) if temporal_node.left_child
+    queue.enqueue(temporal_node.right_child) if temporal_node.right_child
+  end
+  count
+end
+
 # TEST DRIVE
 #      1
 #    /    \
@@ -24,6 +40,12 @@ b = Node.new(3, e, f)
 a = Node.new(2, c, d)
 root = Node.new(1, a, b)
 
+puts "Test Drive using the DFS solution"
 p size_of_binary_tree_DFS(root) == 7
 p size_of_binary_tree_DFS(a) == 3
 p size_of_binary_tree_DFS(d) == 1
+
+puts "\nTest Drive using the BFS (level order) solution"
+p size_of_binary_tree_BFS(root) == 7
+p size_of_binary_tree_BFS(a) == 3
+p size_of_binary_tree_BFS(d) == 1
