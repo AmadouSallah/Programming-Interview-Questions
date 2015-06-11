32cb31fd89362551aba9d69ca5fc03bf8661bf9c
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-06-11 14:46:17 -0700

    Added problem statement

---
+++ trees/level_order_in_reverse.rb
@@ -0,0 +1,7 @@
+# Problem: Write a program that takes a binary tree root and return a reversed level order traversal.
+# Example, for the binary tree below, the output will be: 4567231
+#      1
+#    /    \
+#   2      3
+#  / \    / \
+# 4   5  6   7
