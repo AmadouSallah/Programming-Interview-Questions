2267bc088239efbd4be4dac0c46435e31af81c91
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-06-11 21:48:45 -0700

    Added problem statement with example

---
+++ trees/maximum_width_of_binary_tree.rb
@@ -0,0 +1,13 @@
+# Problem: Given a binary tree, write a program to find the maximum width (also called diameter) of the tree.
+# The width of a tree is the maximum of widths of all levels
+# Example
+#      1
+#    /    \
+#   2      3
+#  / \    /
+# 4   5  6
+#       /
+#      8
+
+# For the binary tree above, width of level 1 = 1, width of level 2 = 2, width of level 3 = 3, and width of level 4 = 1.
+# The program would therefore return 3 for the above tree
