b053cda2763ebcd565f38cf34fd4ba499f650bf1
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-06-12 22:57:30 -0700

    Added problem statement

---
+++ trees/convert_to_mirror.rb
@@ -0,0 +1,3 @@
+# Problem: Write a program to convert a Binary Tree into its Mirror Tree
+# A mirror of a Binary Tree T is another Binary Tree M(T) with left and right children of all non-leaf nodes interchanged.
+
