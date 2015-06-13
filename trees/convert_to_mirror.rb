5dfb0e0c6f8a03bc2d7a674fd38222036a73c3f9
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-06-12 23:01:02 -0700

    Added a binary tree example with expected output

--- trees/convert_to_mirror.rb
+++ trees/convert_to_mirror.rb
@@ -1,3 +1,21 @@
 # Problem: Write a program to convert a Binary Tree into its Mirror Tree
 # A mirror of a Binary Tree T is another Binary Tree M(T) with left and right children of all non-leaf nodes interchanged.
+# Example:
+#      1
+#    /    \
+#   2      3
+#  / \    /
+# 4   5  6
+#       /
+#      8
+
+# becomes
+#      1
+#    /    \
+#   3      2
+#  / \      \
+# 5   4      6
+#              \
+#                8
+
 
