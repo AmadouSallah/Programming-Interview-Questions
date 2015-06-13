ff6ac235b5ac81919cabb64c3122b39a99a33091
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-06-12 23:14:05 -0700

    Added solution to convert_to_mirror.rb

--- trees/convert_to_mirror.rb
+++ trees/convert_to_mirror.rb
@@ -1,3 +1,5 @@
+require_relative 'binary_trees'
+
 # Problem: Write a program to convert a Binary Tree into its Mirror Tree
 # A mirror of a Binary Tree T is another Binary Tree M(T) with left and right children of all non-leaf nodes interchanged.
 # Example:
@@ -13,9 +15,33 @@
 #      1
 #    /    \
 #   3      2
-#  / \      \
-# 5   4      6
-#              \
-#                8
+#    \    / \
+#     6  5  4
+#       \
+#         8
+def convert_binary_tree_to_mirror(root)
+  return if !root
+  temp_node = Node.new()
+  # recursively call the method on both left and right subtrees
+  convert_binary_tree_to_mirror(root.left_child)
+  convert_binary_tree_to_mirror(root.right_child)
 
+  # swap the left and right subtrees
+  temp_node = root.left_child
+  root.left_child = root.right_child
+  root.right_child = temp_node
 
+  return root
+end
+
+h = Node.new(8)
+f = Node.new(6, h)
+e = Node.new(5)
+d = Node.new(4)
+c = Node.new(3, f)
+b = Node.new(2, d, e)
+a = Node.new(1, b, c)
+
+root = a
+
+p convert_binary_tree_to_mirror(root)
