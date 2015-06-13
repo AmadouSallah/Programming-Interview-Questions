e7eed9ca5cdeec3917e49a3f780de863d9e0eefc
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-06-12 23:22:04 -0700

    Added example of mirror trees

--- trees/are_mirrors.rb
+++ trees/are_mirrors.rb
@@ -1,2 +1,20 @@
 # Problem: Write a program to check if 2 binary trees are mirrors of each other
 
+# Example:
+# tree1
+#      1
+#    /    \
+#   2      3
+#  / \    /
+# 4   5  6
+#       /
+#      8
+# tree2
+#      1
+#    /    \
+#   3      2
+#    \    / \
+#     6  5  4
+#       \
+#         8
+# output should be true
