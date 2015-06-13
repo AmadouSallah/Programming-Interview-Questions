244922482e8fd8dafe3757bb3ae99dca9f9d407b
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-06-12 22:46:19 -0700

    Added solution of sum_of_all_elements.rb using Breath First Search

--- trees/sum_of_all_elements.rb
+++ trees/sum_of_all_elements.rb
@@ -1,4 +1,5 @@
 require_relative 'binary_trees'
+require_relative './../stacks_and_queues/queue/implement_queue'
 
 # Problem: Give an algorithm for finding the sum of all elements in binary tree
 
@@ -6,6 +7,22 @@ require_relative 'binary_trees'
 def sum_of_all_elements_DFS(root)
   return 0 if !root
   return root.value + sum_of_all_elements_DFS(root.left_child) + sum_of_all_elements_DFS(root.right_child)
+end
+
+# SOLUTION 2: BFS
+def sum_of_all_elements_BFS(root)
+  return 0 if !root
+  sum = 0
+  queue = Queue.new()
+  queue.enqueue(root)
+  while !queue.is_empty?
+    node = queue.dequeue
+    sum += node.value
+
+    queue.enqueue(node.left_child) if node.left_child
+    queue.enqueue(node.right_child) if node.right_child
+  end
+  sum
 end
 
 #      1
@@ -26,7 +43,14 @@ a = Node.new(1, b, c)
 
 root = a
 
+puts "Using DFS method:"
 p sum_of_all_elements_DFS(root) == 29
 p sum_of_all_elements_DFS(h) == 8
 p sum_of_all_elements_DFS(f) == 14
 p sum_of_all_elements_DFS(nil) == 0
+
+puts "Using BFS method:"
+p sum_of_all_elements_BFS(root) == 29
+p sum_of_all_elements_BFS(h) == 8
+p sum_of_all_elements_BFS(f) == 14
+p sum_of_all_elements_BFS(nil) == 0
