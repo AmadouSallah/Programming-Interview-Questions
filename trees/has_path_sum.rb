11799e74f238290c5a51056f9230d8c4f7af7770
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-06-12 21:18:27 -0700

    Added problem statement

---
+++ trees/has_path_sum.rb
@@ -0,0 +1,3 @@
+# Problem: Given a binary tree and a number, return true if the tree has a root-to-leaf path
+# such that adding up all the values along the path equals the given number.
+# Return false if no such path can be found.
