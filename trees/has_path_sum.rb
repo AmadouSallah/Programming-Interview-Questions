5a46f6dee36e65334f09e69456af9cd683b729a0
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-06-12 21:22:37 -0700

    Added example with expected output

--- trees/has_path_sum.rb
+++ trees/has_path_sum.rb
@@ -1,3 +1,14 @@
 # Problem: Given a binary tree and a number, return true if the tree has a root-to-leaf path
 # such that adding up all the values along the path equals the given number.
 # Return false if no such path can be found.
+
+# Example, for the binary tree below,
+#      1
+#    /    \
+#   2      3
+#  / \    /
+# 4   5  6
+#       /
+#      8
+# the root to leaf path sums are: 7 => 1 2 4; 8 => 1 2 5; 18 => 1 3 6 8
+# The function will return true for numbers 7, 8, or 18, and false otherwise
