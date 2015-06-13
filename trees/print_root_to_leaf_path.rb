f324f11cdaad4073237cf7c8c7a545990a5361b0
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-06-12 19:49:41 -0700

    Added an input example with the expected output

---
+++ trees/print_root_to_leaf_path.rb
@@ -0,0 +1,15 @@
+require_relative 'binary_trees'
+
+# Problem: Given a binary tree, print out all of its root-to-leaf paths one per line
+# Example for the binary tree below,
+#      1
+#    /    \
+#   2      3
+#  / \    /
+# 4   5  6
+#       /
+#      8
+# the output will be:
+# 1 2 4
+# 1 2 5
+# 1 3 6 8
