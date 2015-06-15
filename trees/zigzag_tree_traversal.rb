58e0b700eeb225f5e478ea3cc20352d4b6cc9a4e
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-06-15 15:08:49 -0700

    Added problem statement with example

---
+++ trees/zigzag_tree_traversal.rb
@@ -0,0 +1,14 @@
+# Given the root of a binary tree, print out the tree in zig zag level order
+# (ie, from left to right, then right to left for the next level and alternate between).
+# Output a newline after the end of each level.
+
+#      1
+#    /    \
+#   2      3
+#  / \    / \
+# 4   5  6   7
+# Example, for the tree above, the zig zag level order output is:
+# 1
+# 3 2
+# 4 5 6 7
+
