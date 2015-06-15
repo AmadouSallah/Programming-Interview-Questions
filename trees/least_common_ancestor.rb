4cce7d3caf70cda75f3e6d159780fc8cff21d062
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-06-14 18:52:37 -0700

    Added problem statement of least_common_ancestor.rb

---
+++ trees/least_common_ancestor.rb
@@ -0,0 +1,10 @@
+# Problem: Given a binary tree and two nodes, write a program to find the least common ancestor (LCA).
+# Example: For the tree below,
+#      1
+#    /    \
+#   2      3
+#  / \    /
+# 4   5  6
+#       /
+#      8
+# LCA(8, 5) = 1, LCA(1,6) = 1, LCA(4,5) = 2
