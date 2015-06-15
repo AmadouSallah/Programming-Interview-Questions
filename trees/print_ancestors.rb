ea2baf06f9cb3576531691b96742330331c6d84a
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-06-14 19:33:49 -0700

    Added problem statement for print_ancestors.rb

---
+++ trees/print_ancestors.rb
@@ -0,0 +1,10 @@
+# Problem: Given a Binary Tree and a key node, write a function that prints all the ancestors of the key in the given binary tree.
+# Example, for the binary tree below and key node 8, the ouput is: 6, 3, 1
+#      1
+#    /    \
+#   2      3
+#  / \    /
+# 4   5  6
+#       /
+#      8
+
