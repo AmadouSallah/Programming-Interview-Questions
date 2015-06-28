32377ebb9ec4e84e233c2c6bef7523f3fcb00030
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-06-28 15:00:06 -0700

    Replaced raise errors with a return of -1 in implement_binary_heap.rb file

--- heaps_and_priority_queues/implement_binary_heap.rb
+++ heaps_and_priority_queues/implement_binary_heap.rb
@@ -7,24 +7,33 @@ class BinaryHeap
     @heap_array = []
   end
 
-  # returns the index of the parent of i
+  # returns the index of the parent of i if 0 < i < @size; otherwise returns -1
   def parent(i)
-    raise "Your input index should neither be <=0, nor >= size of heap - Out of range" if i <= 0 || i >= @size
-    (i - 1) / 2
+    if i <= 0 || i >= @size
+      return -1
+    else
+      return (i - 1) / 2
+    end
   end
 
-  # returns the index of the left child of i
+  # returns the index of the left child of i if
   def left_child(i)
     left = (2 * i) + 1
-    raise "The element at index #{i} doesn't have a left child; sorry" if left >= @size
-    left
+    if left >= @size
+      return -1
+    else
+      return left
+    end
   end
 
   # returns the index of the right child of i
   def right_child(i)
-    right =2 * (i + 2)
-    raise "The element at index #{i} doesn't have a right child; sorry" if right >= @size
-    right
+    right = 2 * (i + 2)
+    if right >= @size
+      return -1
+    else
+      return right
+    end
   end
 
   # returns the value of the left child of the element at index i
@@ -40,5 +49,7 @@ class BinaryHeap
 end
 
 
-# heap = BinaryHeap.new()
-# p heap.right_child_key(0)
+heap = BinaryHeap.new()
+p heap.left_child(1) == -1
+p heap.right_child(3) == -1
+p heap.right_child_key(2) == nil
