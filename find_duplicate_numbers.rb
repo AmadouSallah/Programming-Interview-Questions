e691085711a548a018be9fab9fbef0f97e154810
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-07-01 16:14:48 -0700

    Added second, most efficient solution for find_duplicate_numbers.rb - 0(n) running time, 0(m) space where m is the largest element of the input array

--- find_duplicate_numbers.rb
+++ find_duplicate_numbers.rb
@@ -21,9 +21,40 @@ def find_duplicate_numbers(array)
   result
 end
 
+# SOLUTION 2
+def find_duplicate_numbers_2(array)
+  result = []
+
+  # Find the largest value in array
+  max = array[0]
+  array.each {|elt| max = elt if elt > max}
+
+  # Create a boolean array of length max whose values are all initially set to false
+  boolean = Array.new(max) {false}
+
+  i = 0
+  while i < array.length
+    element = array[i]
+    if boolean[element] && !result.include?(element)
+      result << element
+    else
+      boolean[element] = true
+    end
+    i += 1
+  end
+  result
+end
+
 # TEST DRIVE
 
 puts "Testing brute force solution:"
 p find_duplicate_numbers([1,3,2,1,2,9,7,9]) == [1, 2, 9]
 p find_duplicate_numbers([1,2,3]) == []
 p find_duplicate_numbers([0, 0, 1]) == [0]
+p find_duplicate_numbers([25]) == []
+
+puts "\nTesting the second, more efficient solution:"
+p find_duplicate_numbers_2([1,3,2,1,2,9,7,9]) == [1, 2, 9]
+p find_duplicate_numbers_2([1,2,3]) == []
+p find_duplicate_numbers_2([0, 0, 1]) == [0]
+p find_duplicate_numbers_2([8]) == []
