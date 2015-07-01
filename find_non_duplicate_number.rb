218a19c8a633eb5189a4932e2a7d79106de92c58
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-06-30 18:47:58 -0700

    Added an example with expected output

--- find_non_duplicate_number.rb
+++ find_non_duplicate_number.rb
@@ -1,1 +1,2 @@
 # Given an array that contains many duplicates integers and one unique integer, write a program to find and return the unique integer.
+# Example, for input = [1, 4, 1, 7, 4], the output will be 7
