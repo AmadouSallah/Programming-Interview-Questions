a1ed1620c3e89116d1c2068ad1c7cc4beeb50b99
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-07-01 12:40:37 -0700

    Added Solution 2 which uses a hash table: 0(n) running time and 0(n) space

--- find_non_duplicate_number.rb
+++ find_non_duplicate_number.rb
@@ -31,8 +31,27 @@ def find_non_duplicate_integer_1(array)
   array[i]
 end
 
+# SOLUTION 2: Using a hash table - 0(n) running time and 0(n) space
+def find_non_duplicate_integer_2(array)
+  hash = Hash.new(0)
+  array.each do |elt|
+    hash[elt] += 1
+  end
+
+  # return the hash key with a value of 1
+  hash.each {|key, value| return key if value == 1}
+end
+
 # TEST DRIVE
+
+puts "Testing Solution 1:"
 p find_non_duplicate_integer_1([7]) == 7
 p find_non_duplicate_integer_1([7, 1, 4, 1, 4]) == 7
 p find_non_duplicate_integer_1([1, 4, 3, 1, 4]) == 3
 p find_non_duplicate_integer_1([1, 4, 1, 4, 0]) == 0
+
+puts "\nTesting solution 2:"
+p find_non_duplicate_integer_2([7]) == 7
+p find_non_duplicate_integer_2([7, 1, 4, 1, 4]) == 7
+p find_non_duplicate_integer_2([1, 4, 3, 1, 4]) == 3
+p find_non_duplicate_integer_2([1, 4, 1, 4, 0]) == 0
