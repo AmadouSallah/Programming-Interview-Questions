bbd1951bb0d6b8ec627b4cb970ecb97431a73691
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-07-02 14:58:48 -0700

    Added a 2nd solution to find_2nd_largest_element.rb - 0(n) running time and 0(1) space

--- find_2nd_largest_element.rb
+++ find_2nd_largest_element.rb
@@ -7,10 +7,33 @@ def find_2nd_largest_element(array)
   sorted[-2] # returns the 2nd to last element
 end
 
+# SOLUTION 2: 0(n) running time and 0(1) space complexities
+def find_2nd_largest_element_2(array)
+  len = array.length
+  return nil if len < 2
+
+  largest, second_largest = array[0], array[0]
+  array.each do |element|
+    if element > largest
+      second_largest = largest
+      largest = element
+    elsif element > second_largest
+      second_largest = element
+    end
+  end
+
+  second_largest
+end
 
 # TEST CASE:
+puts "Testing Solution 1:"
 p find_2nd_largest_element([1, -4, 3, 7, 9, 0]) == 7
 p find_2nd_largest_element([]) == nil
 p find_2nd_largest_element([1]) == nil
 p find_2nd_largest_element([1, 1]) == 1
 
+puts "\nTesting Solution 2:"
+p find_2nd_largest_element_2([1, -4, 3, 7, 9, 0]) == 7
+p find_2nd_largest_element_2([]) == nil
+p find_2nd_largest_element_2([1]) == nil
+p find_2nd_largest_element_2([1, 1]) == 1
