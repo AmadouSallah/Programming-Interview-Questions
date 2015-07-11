7563747fb9a1b8fb6a3626210184367d6d9020ea
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-07-10 18:57:03 -0700

    Added recursive solution - 0(n) running time and 0(n) space complexity

--- recursion_and_dynamic_programming/factorial.rb
+++ recursion_and_dynamic_programming/factorial.rb
@@ -3,3 +3,18 @@
 # 1! = 1
 # n! = n*(n-1)! for all n > 0
 
+# SOLUTION 1: recursion - 0(n) running time and 0(n) space complexity
+def factorial_recursive(n)
+  return nil if n < 0
+
+  if n < 2 # Base case
+    return n
+  else # Recursive case
+    return n * factorial_recursive(n-1)
+  end
+
+end
+
+# TEST CASES
+p factorial_recursive(10) == 3628800
+# p factorial_recursive(1000000) # Stactk too deep :(
