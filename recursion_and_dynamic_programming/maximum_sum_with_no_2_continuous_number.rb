4e1ba0f4992ea03d32a86c59c9e77711fe04dd96
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-07-11 16:39:08 -0700

    Added problem statement for maximum_sum_with_no_2_continuous_number.rb

---
+++ recursion_and_dynamic_programming/maximum_sum_with_no_2_continuous_number.rb
@@ -0,0 +1,8 @@
+# Problem: Given an array of numbers (not all negative), write a program to find the
+# maximum sum of a subsequence with the constraint that no 2 numbers in the sequence
+# should be adjacent in the array
+# Answer the question in the most efficient way.
+
+# Example:
+# [3, 2, 7, 10] should return 13 (sum of 3 and 10);
+# [3, 2, 5, 10, 7] should return 15 (sum of 3, 5 and 7).
