208916bccc13965fe6316392d0d5dfcdbfe48aa9
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-07-11 14:10:44 -0700

    Added Solution 2 using Dynamic Programming - 0(n) running time and 0(n) space complexities

--- recursion_and_dynamic_programming/maximum_value_contiguous_subsequence.rb
+++ recursion_and_dynamic_programming/maximum_value_contiguous_subsequence.rb
@@ -19,7 +19,31 @@ def maximum_value_contiguous_subsequence(array)
   max
 end
 
+# SOLUTION 2: Using Dynamic Programming - 0(n) running time and 0(n) space complexities
+def maximum_value_contiguous_subsequence_dp(array)
+  len = array.length
+  result = []
+  # if the 1st array[0] is positive, put it in the result array, otherwise put 0
+  result[0] = (array[0] > 0) ? array[0] : 0
+
+  array.each_with_index do |element, index|
+    result[index] = (result[index-1] + element > 0) ? (result[index-1] + element) : 0
+  end
+
+  max = 0
+  result.each do |element|
+    max = (max > element) ? max : element
+  end
+
+  max
+end
+
 # TEST DRIVE
 p maximum_value_contiguous_subsequence([1, -3, 4, -2, -1, 6]) == 7
 p maximum_value_contiguous_subsequence([1, -5, 2, -1, 3]) == 4
 p maximum_value_contiguous_subsequence([-2, 11, -4, 13, -5, 2]) == 20
+
+puts "Solution 2 using Dynamic Programming:"
+p maximum_value_contiguous_subsequence_dp([1, -3, 4, -2, -1, 6]) == 7
+p maximum_value_contiguous_subsequence_dp([1, -5, 2, -1, 3]) == 4
+p maximum_value_contiguous_subsequence_dp([-2, 11, -4, 13, -5, 2]) == 20
