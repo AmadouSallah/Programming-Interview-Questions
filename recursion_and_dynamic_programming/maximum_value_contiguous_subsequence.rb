b0b659553060c623129c3116bf7526af02bcae92
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-07-11 13:00:08 -0700

    Added problem statement

---
+++ recursion_and_dynamic_programming/maximum_value_contiguous_subsequence.rb
@@ -0,0 +1,5 @@
+# Problem: Given an array of n numbers, determine a contiguous subsequence a[i]...a[j] for which the sum if elements is maximum
+# Example:
+# input: [1, -3, 4, -2, -1, 6] => output = 7
+# input = [1, -5, 2, -1, 3] => output = 4
+# input = [-2, 11, -4, 13, -5, 2] => output = 20
