e9ca67b21b0b800165cb283d74ba9778985d16c4
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-07-11 18:56:04 -0700

    Added problem statement for maximum_sum_with_no_3_continuous_numbers.rb

---
+++ recursion_and_dynamic_programming/maximum_sum_with_no_3_continuous_numbers.rb
@@ -0,0 +1,4 @@
+# Problem: Given an array of numbers (not all negative), write a program to find the
+# maximum sum of a subsequence with the constraint that no 3 numbers in the sequence
+# should be adjacent in the array
+# Answer the question in the most efficient way.
