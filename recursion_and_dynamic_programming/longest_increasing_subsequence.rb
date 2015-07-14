2a17f775acdd452ee2cb4fa821d218a27665888d
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-07-13 19:46:59 -0700

    Added problem statement

---
+++ recursion_and_dynamic_programming/longest_increasing_subsequence.rb
@@ -0,0 +1,6 @@
+# Problem: Given a sequence of numbers, write a program to find a subsequence (not necessarily contiguous)
+# whose elements are in sorted order, lowest to highest, and in which the subsequence is as long as possible.
+
+# Example:
+# nums = [3, 4, -1, 0, 6, 2, 3] => output = [-1, 0, 2,3]
+# nums = [0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15] => output = [0, 2, 6, 9, 11, 15]
