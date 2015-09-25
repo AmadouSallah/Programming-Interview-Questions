ec727c26749282647bb600aaec2a7011653feab5
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-09-24 20:29:33 -0700

    Changed the implementation of the stack

--- stacks_and_queues/stacks/implement_stack.rb
+++ stacks_and_queues/stacks/implement_stack.rb
@@ -3,6 +3,64 @@
 # The last element  inserted is the first one to be deleted => Last In first Out (LIFO)
 
 class Stack
+
+  attr_reader :container, :stack_size
+  def initialize(stack_size)
+    @container = []
+    @stack_size = stack_size
+  end
+
+  # adds an element to the top (end) of the stack
+  def push(element)
+    raise "Stack Overflow! - The Stack is full" if @container.length > @stack_size - 1
+    @container << element
+  end
+
+  # Returns the top stack element if the stack is not empty.
+  def peek
+    raise "Stack Underflow - The Stack is empty!" if self.is_empty?
+    @container.last
+  end
+
+  # Removes and returns the top stack element if the stack is not empty.
+  def pop
+    raise "Stack Underflow - The Stack is empty!" if self.is_empty?
+    @container.pop
+  end
+
+  # Returns true if the stack is empty, and false otherwise
+  def is_empty?
+    @container.length == 0
+  end
+
+  # Returns true if the stack is full, and false otherwise
+  def is_full?
+    @container.length == @stack_size
+  end
+end
+
+myStack = Stack.new(2)
+
+p myStack.is_empty? == true
+p myStack.is_full? == false
+
+myStack.push(1)
+p myStack.container == [1]
+
+myStack.push(2)
+p myStack.container == [1, 2]
+
+p myStack.is_empty? == false
+p myStack.is_full? == true
+
+p myStack.peek == 2
+myStack.pop
+
+p myStack.container == [1]
+
+######################################
+=begin
+class Stack
   attr_accessor :store, :top
   def initialize
     @store = []
@@ -53,3 +111,6 @@ end
 
 # puts "\nRemoving the last element from the stack #{myStack.pop}"
 # puts "The stack in now #{myStack.store} and the last element of the stack is #{myStack.peek}; the size of the stack is now #{myStack.size}"
+
+=end
+
