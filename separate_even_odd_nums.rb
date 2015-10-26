bb9b01d34eaf4eea46fec2524b3eedfea000633c
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-10-26 15:18:06 -0700

    Added pseudocode to separate_even_odd_nums.rb

--- separate_even_odd_nums.rb
+++ separate_even_odd_nums.rb
@@ -5,3 +5,12 @@ Example: input = [12, 34, 45, 9, 8, 90, 3], output = [12, 34, 90, 8, 9, 45, 3]
 In the output, the order of numbers can be changed
 
 =end
+
+=begin
+      PSEUDOCODE:
+1) We initialize 2 variables, left_index = 0 and right_index = array.length-1
+2) We increment left_index until we encounter an odd number.
+3) We decrement right_index until we encounter an even number.
+4) We swap the array elements at left_index and right_index if left_index < right_index
+
+=end
