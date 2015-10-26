d832116bdb99d59e3b3782285c97472100ddf613
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-10-26 15:17:23 -0700

    Added problem statement

---
+++ separate_even_odd_nums.rb
@@ -0,0 +1,7 @@
+=begin
+Given an array of numbers, write a function that segregates even and odd numbers from the array. The function should put all even numbers first and then odd numbers.
+The function shouldn't create a new array.
+Example: input = [12, 34, 45, 9, 8, 90, 3], output = [12, 34, 90, 8, 9, 45, 3]
+In the output, the order of numbers can be changed
+
+=end
