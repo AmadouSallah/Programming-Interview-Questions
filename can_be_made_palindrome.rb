b1d6770a97f55b6186f263f4a32501f1a0fb3dd5
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-11-23 08:36:06 -0800

    Added solution to can_be_made_palindrome.rb

--- can_be_made_palindrome.rb
+++ can_be_made_palindrome.rb
@@ -1,2 +1,16 @@
 # Write an efficient function that checks whether any permutation of an input string is a palindrome
 
+def can_be_made_palindrome?(string)
+  return true if string.length < 2
+
+  hash = Hash.new(0)
+  string.chars.each {|key| hash[key] += 1}
+
+  number_of_odd_chars = 0
+  hash.values.each do |value|
+    number_of_odd_chars += 1 if value % 2 != 0
+    return false if number_of_odd_chars > 1
+  end
+  true
+end
+
