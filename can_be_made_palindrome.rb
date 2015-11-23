24fe94534b83ff19a0c9a4d8f08df4a5f8176f54
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-11-23 08:45:14 -0800

    Added comments

--- can_be_made_palindrome.rb
+++ can_be_made_palindrome.rb
@@ -6,11 +6,14 @@ def can_be_made_palindrome?(string)
   hash = Hash.new(0)
   string.chars.each {|key| hash[key] += 1}
 
-  number_of_odd_chars = 0
+  number_of_odd_chars = 0 # will hold the number of keys whose value is odd
   hash.values.each do |value|
     number_of_odd_chars += 1 if value % 2 != 0
+    # All the hash keys except one should have an even value for the string to be a palindrome.
+    # Therefore we return false whenever number_of_odd_chars is > 1.
     return false if number_of_odd_chars > 1
   end
+  # We return true since we didn't find more than 1 character whose hash value is > 1
   true
 end
 
