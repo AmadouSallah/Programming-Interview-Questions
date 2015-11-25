beb8c882d8793384536bcf77eddf4e69e9360cba
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-11-25 08:59:41 -0800

    Added find_index_of_closing_parenthesis.rb file

---
+++ find_index_of_closing_parenthesis.rb
@@ -0,0 +1,7 @@
+=begin
+
+Write a function that takes a string and a position of an opening parenthesis.
+It should return the index of the corresponding closing parenthesis.
+If no such closing parenthesis is found, the function should return -1
+
+=end
