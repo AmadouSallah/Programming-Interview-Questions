4579aa3d10a4468c49bcb28fd18b7899045cd4e6
AmadouSallah <amadou.saikou.sallah@gmail.com>
2016-01-02 19:42:24 -0800

    Added problem statement for find_duplicates_in_efficient_way.rb

---
+++ find_duplicates_in_efficient_way.rb
@@ -0,0 +1,8 @@
+=begin
+
+Problem: Find duplicates in O(n) time and O(1) extra space
+Given an array of n elements which contains elements from 0 to n-1, with any of these numbers appearing any number of times. Find these repeating numbers in O(n) and using only constant memory space - 0(1).
+
+For example, let n be 7 and array be [1, 2, 3, 1, 3, 6, 6], the answer should be 1, 3 and 6
+
+=end
