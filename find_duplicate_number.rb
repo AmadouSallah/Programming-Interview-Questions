6838ccb5a4aad74e45c6adf2645c66b0d78719df
AmadouSallah <amadou.saikou.sallah@gmail.com>
2016-01-03 19:32:41 -0800

    Added find_duplicate_number.rb file and problem statement

---
+++ find_duplicate_number.rb
@@ -0,0 +1,6 @@
+=begin
+
+Suppose you have an array of n+1 integers. The integers are in random order, but you know each of the integers is between 1 and n (inclusive). In addition, each number appears only once in the array, except for one number, which occurs twice. Describe an algorithm to find the repeated number in the most efficient way, that is in
+0(n) time and 0(1) space.
+
+=end
