b3da5cce9308c2e5eb1b6cc5821158b0cac194a3
AmadouSallah <amadou.saikou.sallah@gmail.com>
2016-01-03 19:51:32 -0800

    Added solution of find_duplicate_number.rb

--- find_duplicate_number.rb
+++ find_duplicate_number.rb
@@ -4,3 +4,26 @@ Suppose you have an array of n+1 integers. The integers are in random order, but
 0(n) time and 0(1) space.
 
 =end
+
+# PSEUDOCODE:
+# We initialize result to 0. We then xor all the array elements
+# with the indexes from 0 to n (there are n+1 elements) and put it to result.
+# This would result to the duplicate element since it is the only
+# element that ends up being xor'ed 3 times. All the other elements
+# get xor'ed twice, which is 0. We return result
+
+# Example, if array = [1, 4, 2, 1, 3], we get:
+# result = 0^(1^0)^(4^1)^(2^2)^(1^3)^(3^4)
+#        = (1^1^1)^(2^2)^(3^3)^(4^4) = 1
+
+def find_duplicate(nums)
+  result = 0
+  nums.each_with_index do |num, index|
+    result ^= num ^ index
+  end
+  result
+end
+
+# TEST CASE
+p find_duplicate([1,4,2,1,3]) == 1
+p find_duplicate([5, 2, 1, 2, 3, 4]) == 2
