0987c82229ef117f58fc64254df2ed1ec4a9f368
AmadouSallah <amadou.saikou.sallah@gmail.com>
2016-01-04 21:43:15 -0800

    Added solution1 to prob_1_two_sum.rb: 0(n^2) running time and 0(1) space complexities

--- leetcode/ruby/prob_1_two_sum.rb
+++ leetcode/ruby/prob_1_two_sum.rb
@@ -11,3 +11,30 @@ Input: numbers={2, 7, 11, 15}, target=9
 Output: index1=1, index2=2
 
 =end
+
+# SOLUTION 1: 0(n^2) running time and 0(1) space
+def two_sum1(numbers, target)
+  len = numbers.length
+
+  i = 0
+  while i < len
+    first_element = numbers[i]
+    j = i+1
+    while j < len
+      second_element = numbers[j]
+      if first_element + second_element == target
+        puts "index1=#{i+1}, index2=#{j+1}"
+        return
+      end
+      j +=1
+    end
+    i +=1
+  end
+end
+
+# TEST CASES
+two_sum1([2, 7, 11, 15], 9) # index1=1, index2=2
+two_sum1([2, 7, 11, 15], 13) # index1=1, index2=3
+two_sum1([2, 7, 11, 15], 17) # index1=1, index2=4
+two_sum1([2, 7, 11, 15], 18) # index1=2, index2=3
+two_sum1([2, 7, 11, 15], 22) # index1=2, index2=4
