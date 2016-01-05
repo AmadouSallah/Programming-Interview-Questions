76c24dba46bec117516283e343573a6b313116fe
AmadouSallah <amadou.saikou.sallah@gmail.com>
2016-01-04 22:37:16 -0800

    Added solution2: 0(n) running time and 0(n) space complexities

--- leetcode/ruby/prob_1_two_sum.rb
+++ leetcode/ruby/prob_1_two_sum.rb
@@ -39,9 +39,49 @@ def two_sum1(numbers, target)
   end
 end
 
+# SOLUTION 2: 0(n) running time and 0(n) space complexities
+
+# Pseudocode:
+# We initialize an empty hash.
+# We go through the array (numbers) once. For each element, we set 2 variables, num1 &  num2:
+# num1 represent the difference between "target" and the current element whereas num2 represent the
+# current element. The hash will have as key-value pairs num1 => num2; i.e. for each
+# num1 and num2, hash[num1] = num2.
+# As we traverse the array, for each element, we check if key num2 alreay exists in the hash.
+# If so, that would signal that we have found the second element that we are looking for
+# and that the 1st element is just num1 since num1 +  num2 = target. We then print the indexes and return.
+# Otherwise (key num2 is not in the hash), we set the hash value of num1 to be num2
+
+def two_sum2(numbers, target)
+  len = numbers.length
+  my_hash = {}
+
+  i = 0
+  while i < len
+    num2 = numbers[i]
+    num1 = target - num2
+
+    if my_hash[num2] # A match is found since num1 + num2 = target
+      index1 = numbers.index(num1) + 1
+      index2 = i + 1
+      puts "index1=#{index1}, index2=#{index2}"
+      return
+    else
+      my_hash[num1] = num2
+    end
+    i += 1
+  end
+end
+
 # TEST CASES
 two_sum1([2, 7, 11, 15], 9) # index1=1, index2=2
 two_sum1([2, 7, 11, 15], 13) # index1=1, index2=3
 two_sum1([2, 7, 11, 15], 17) # index1=1, index2=4
 two_sum1([2, 7, 11, 15], 18) # index1=2, index2=3
 two_sum1([2, 7, 11, 15], 22) # index1=2, index2=4
+puts
+two_sum2([2, 7, 11, 15], 9) # index1=1, index2=2
+two_sum2([2, 7, 11, 15], 13) # index1=1, index2=3
+two_sum2([2, 7, 11, 15], 17) # index1=1, index2=4
+two_sum2([2, 7, 11, 15], 18) # index1=2, index2=3
+two_sum2([2, 7, 11, 15], 22) # index1=2, index2=4
