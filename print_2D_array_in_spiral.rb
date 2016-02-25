185f4120fdf5f8b132a610fe7387b03e6107f8f0
AmadouSallah <amadou.saikou.sallah@gmail.com>
2016-02-24 22:30:50 -0800

    Renamed file

---
+++ print_2D_array_in_spiral.rb
@@ -0,0 +1,19 @@
+=begin
+
+Given a 2D array, print it in spiral form.
+Example:
+Input:
+        1    2   3
+        4    5   6
+        7    8   9
+
+Output: 1 2 3 6 9 8 7 4 5
+
+Input:
+        1   2   3   4  5   6
+        7   8   9  10  11  12
+        13  14  15 16  17  18
+
+Output: 1 2 3 4 5 6 12 18 17 16 15 14 13 7 8 9 10 11
+
+=end
