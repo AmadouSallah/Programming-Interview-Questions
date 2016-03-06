2c4d69710ef19a8a1f4036292209fd1e5ba39580
AmadouSallah <amadou.saikou.sallah@gmail.com>
2016-03-05 16:49:05 -0800

    Added updates to twoSum.java

--- leetcode/java/prob1TwoSum/twoSum.java
+++ leetcode/java/prob1TwoSum/twoSum.java
@@ -1,5 +1,88 @@
 /*
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 
+You may assume that each input would have exactly one solution.
+
+Example:
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+*/
+
+import java.util.HashMap;
+
+public class twoSum {
+
+  public static int[] towSum(int[] nums, int target) {
+
+    if ( (nums == null) || (nums.length == 0) ) {
+      return new int[0];
+    }
+
+    int n = nums.length;
+    int[] result = new int[2];
+    HashMap<Integer, Integer> hashmap = new HashMap<Integer, Integer>();
+
+    for (int i = 0; i < n; i++) {
+
+      int currentElement = nums[i];
+      int difference = target - currentElement;
+
+      if (hashmap.containsKey(difference)) {
+
+        result[0] = hashmap.get(difference);
+        result[1] = i;
+        break;
+
+      } else {
+        hashmap.put(currentElement, i);
+      }
+    }
+
+    return result;
+  }
+
+  public static String printArray(int[] arr) {
+    if ((arr == null) || (arr.length == 0)) {
+      return "[]";
+    }
+
+    String result = "[";
+    int n = arr.length;
+    for (int i = 0; i < n; i++) {
+      result += arr[i];
+      if (i < n-1) {
+        result += ", ";
+      }
+    }
+    result += "]";
+
+    return result;
+  }
+
+  public static void main(String[] args) {
+    int[] arr1 = {};
+    int[] arr2 = {1};
+    int[] arr3 = {2, 7};
+    int[] arr4 = {2, 15, 11, 7};
+    int target = 9;
+
+    int[] r1 = towSum(arr1, target);
+    int[] r2 = towSum(arr2, target);
+    int[] r3 = towSum(arr3, target);
+    int[] r4 = towSum(arr4, target);
+
+    System.out.println(printArray(r1));
+    System.out.println(printArray(r2));
+    System.out.println(printArray(r3));
+    System.out.println(printArray(r4));
+  }
+
+}
+
+/*
+Older one:
 Given an array of integers, find two numbers such that they add up to a specific target number.
 The function twoSum should return indices of the two numbers such that they add up to the target,
 where index1 must be less than index2. Please note that your returned answers
@@ -23,7 +106,7 @@ adds up to the target. If so, these 2 elements are the ones we are looking for.
 We then print each of their indexes + 1 (since it's not zero based, we add 1 to each index).
 
 */
-
+/*
 import java.util.HashMap;
 
 class TwoSum {
@@ -71,5 +154,5 @@ class TwoSum {
     }
   }
 }
-
+*/
 
