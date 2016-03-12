ea28f42120ad0faa458360ca54bba5779c31f9e2
AmadouSallah <amadou.saikou.sallah@gmail.com>
2016-03-12 15:09:33 -0800

    Added solution to StringToInteger.java

--- leetcode/java/prob8StringToInteger/StringToInteger.java
+++ leetcode/java/prob8StringToInteger/StringToInteger.java
@@ -13,3 +13,56 @@ If the first sequence of non-whitespace characters in str is not a valid integra
 If no valid conversion could be performed, a zero value is returned. If the correct value is out of the range of representable values, INT_MAX (2147483647) or INT_MIN (-2147483648) is returned.
 */
 
+public class StringToInteger {
+  public static int myAtoi(String str) {
+
+    if (str == null || str.isEmpty()) return 0;
+
+    str = str.trim();
+
+    long result = 0;
+    int i = 0, sign = 1, len = str.length();
+    int smallestInteger = Integer.MIN_VALUE;
+    int largestInteger = Integer.MAX_VALUE;
+
+    if (str.charAt(i) == '+') {
+      i++;
+    } else if (str.charAt(i) == '-') {
+      sign = -1;
+      i++;
+    }
+
+    for (int j = i; j < len; j++) {
+
+      char currentChar = str.charAt(j);
+
+      /* if the current character is not a digit, return */
+      if (!Character.isDigit(currentChar)) {
+          return (int) result * sign;
+      }
+
+      int currentDigit = currentChar - '0';
+      result = result * 10 + currentDigit;
+
+      /* if result is outside of the range of int values, return 0 */
+      if ( (result > largestInteger) && (sign == 1) ) {
+        return largestInteger;
+      }
+      if (-1 * result < smallestInteger && sign == -1) {
+        return sign * smallestInteger;
+      }
+    }
+
+      return (int) result * sign;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("myAtoi(\"   h-12i \") is: " + myAtoi("   h-12i ") );
+        System.out.println("myAtoi(\"--123\") is: " + myAtoi("--123") );
+        System.out.println("myAtoi(\"+123hi \") is: " + myAtoi("+123hi "));
+        System.out.println("myAtoi(\"+12345\") is: " + myAtoi("+12345"));
+        System.out.println("myAtoi(\"-12345\") is: " + myAtoi("-12345"));
+        System.out.println("myAtoi(\"+2147483648\") is: " + myAtoi("+2147483648"));
+        System.out.println("myAtoi(\"-2147483649\") is: " + myAtoi("-2147483649"));
+    }
+}
