239b5f5fdf27509bc1237b29e9ab3cd177dfadbd
AmadouSallah <amadou.saikou.sallah@gmail.com>
2016-03-16 22:33:57 -0700

    Added Solution with 0(n^2) running time using Dynamic Programming

--- leetcode/java/prob300LongestIncreasingSubsequence/LengthOfLIS.java
+++ leetcode/java/prob300LongestIncreasingSubsequence/LengthOfLIS.java
@@ -14,3 +14,54 @@ Your algorithm should run in O(n2) complexity.
 Follow up: Could you improve it to O(n log n) time complexity?
 */
 
+import java.util.Arrays;
+
+public class LengthOfLIS {
+
+  public static int lengthOfLIS(int[] nums) {
+
+    if (nums == null || nums.length == 0) return 0;
+
+    int len = nums.length;
+
+    if (len == 1) return 1;
+
+    int[] tempArray = new int[len];
+    Arrays.fill(tempArray, 1);
+
+    for (int i = 1; i < len; i++) {
+      for(int j = 0; j < i; j++) {
+        if (nums[i] > nums[j])
+          tempArray[i] = Math.max(tempArray[i], tempArray[j]+1);
+      }
+    }
+
+    int max = 1;
+    for (int i = 0; i < len; i++) {
+      max = Math.max(max, tempArray[i]);
+    }
+    return max;
+  }
+
+  public static void printArray(int[] arr) {
+    String result = "[";
+    int len = arr.length;
+    for (int i = 0; i < len; i++) {
+      result += arr[i];
+      if (i < len-1)
+        result += ", ";
+    }
+    result += "]";
+    System.out.println(result);
+    }
+
+  public static void main(String[] args) {
+    int[] arr = {10, 9, 2, 5, 3, 7, 101, 18};
+    printArray(arr);
+    System.out.println(lengthOfLIS(arr));
+
+    int[] arr1 = {-2, -1};
+    printArray(arr1);
+    System.out.println(lengthOfLIS(arr1));
+  }
+}
