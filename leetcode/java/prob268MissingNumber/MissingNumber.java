f37fd81d61959eb688733e9b3a51c3952acb5460
AmadouSallah <amadou.saikou.sallah@gmail.com>
2016-04-04 00:46:29 -0700

    Added solution of MissingNumber.java

--- leetcode/java/prob268MissingNumber/MissingNumber.java
+++ leetcode/java/prob268MissingNumber/MissingNumber.java
@@ -10,3 +10,50 @@ Note:
 Your algorithm should run in linear runtime complexity. Could you implement it
 using only constant extra space complexity?
 */
+
+public class MissingNumber {
+
+  public static int missingNumber(int[] nums) {
+    int n = nums.length, result = 0;
+    for (int i = 0; i < n; i++)
+      result = result ^ nums[i] ^ i;
+
+    return result ^ n;
+  }
+
+  public static void printArray(int[] arr) {
+    String result = "[";
+    int len = arr.length;
+    for (int i = 0; i < len; i++) {
+      result += arr[i];
+      if (i < len-1)
+        result += ", ";
+    }
+    result += "]";
+    System.out.print(result);
+  }
+
+  public static void main(String[] args) {
+    int[] arr1 = {0};
+    int[] arr2 = {1};
+    int[] arr3 = {0, 1, 3};
+    int[] arr4 = {4, 2, 1, 3};
+
+    System.out.print("For arr1 = ");
+    printArray(arr1);
+    System.out.println(", the missing number is " + missingNumber(arr1));
+
+    System.out.print("For arr2 = ");
+    printArray(arr2);
+    System.out.println(", the missing number is " + missingNumber(arr2));
+
+    System.out.print("For arr3 = ");
+    printArray(arr3);
+    System.out.println(", the missing number is " + missingNumber(arr3));
+
+    System.out.print("For arr4 = ");
+    printArray(arr4);
+    System.out.println(", the missing number is " + missingNumber(arr4));
+
+  }
+}
