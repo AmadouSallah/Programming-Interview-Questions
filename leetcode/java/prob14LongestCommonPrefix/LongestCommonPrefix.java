75d7cc29c64b2f35200fd7e9e2264184f4a18de1
AmadouSallah <amadou.saikou.sallah@gmail.com>
2016-04-24 18:45:51 -0700

    Added solution to LongestCommonPrefix.java

--- leetcode/java/prob14LongestCommonPrefix/LongestCommonPrefix.java
+++ leetcode/java/prob14LongestCommonPrefix/LongestCommonPrefix.java
@@ -4,3 +4,28 @@ Longest Common Prefix
 Write a function to find the longest common prefix string
 amongst an array of strings.
 */
+
+public class LongestCommonPrefix {
+
+  public static String longestCommonPrefix(String[] strs) {
+
+    if (strs == null || strs.length == 0) return "";
+
+    int len = strs.length;
+    String result = strs[0];
+
+    for (int i = 1; i < len; i++) {
+
+      String s = strs[i];
+      int minLen = Math.min(s.length(), result.length());
+      int j = 0;
+
+      while ( (j < minLen) && (result.charAt(j) == s.charAt(j)) )
+        j++;
+
+      result = result.substring(0, j);
+    }
+
+    return result;
+  }
+}
