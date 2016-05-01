3526516517791352b31280bb56522ccadd73439d
AmadouSallah <amadou.saikou.sallah@gmail.com>
2016-04-30 18:08:19 -0700

    Added problem statement for LetterCombinations.java

---
+++ leetcode/java/prob17LetterCombinationsOfAPhoneNumber/LetterCombinations.java
@@ -0,0 +1,11 @@
+/*
+Letter Combinations of a Phone Number
+
+Given a digit string, return all possible letter combinations that the number could represent.
+
+Input:Digit string "23"
+Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+
+Note:
+Although the above answer is in lexicographical order, your answer could be in any order you want.
+*/
