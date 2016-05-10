1c3032a0671c29adb7e4a1ae3ff072db27c5016b
AmadouSallah <amadou.saikou.sallah@gmail.com>
2016-05-10 11:38:50 -0700

    Added problem statement to IsValid.java

---
+++ leetcode/java/prob20IsValidParentheses/IsValid.java
@@ -0,0 +1,10 @@
+/*
+Valid Parentheses
+
+Given a string containing just the characters '(', ')', '{', '}',
+'[' and ']', determine if the input string is valid.
+
+The brackets must close in the correct order, "()" and "()[]{}"
+are all valid but "(]" and "([)]" are not.
+
+*/
