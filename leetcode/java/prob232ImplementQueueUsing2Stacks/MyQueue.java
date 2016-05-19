3c8330bbae0e982554460ac07f845f5186f54ad5
AmadouSallah <amadou.saikou.sallah@gmail.com>
2016-05-19 12:26:11 -0700

    Added solution to MyQueue.java

--- leetcode/java/prob232ImplementQueueUsing2Stacks/MyQueue.java
+++ leetcode/java/prob232ImplementQueueUsing2Stacks/MyQueue.java
@@ -17,3 +17,47 @@ as long as you use only standard operations of a stack.
 You may assume that all operations are valid (for example, no pop
 or peek operations will be called on an empty queue).
 */
+
+import java.util.Stack;
+
+public class MyQueue {
+
+  Stack<Integer> stack1 = new Stack<Integer>();
+  Stack<Integer> stack2 = new Stack<Integer>();
+
+  // Push element x to the back of queue.
+  public void push(int x) {
+    if (stack1.isEmpty()) {
+      while (!stack2.isEmpty()) {
+        stack1.push(stack2.pop());
+      }
+    }
+    stack1.push(x);
+  }
+
+  // Removes the element from in front of queue.
+  public int pop() {
+      if (stack2.isEmpty()) {
+        while (!stack1.isEmpty()) {
+          stack2.push(stack1.pop());
+        }
+      }
+      return stack2.pop();
+  }
+
+  // Get the front element.
+  public int peek() {
+    if (stack2.isEmpty()) {
+      while (!stack1.isEmpty()) {
+        stack2.push(stack1.pop());
+      }
+    }
+    return stack2.peek();
+  }
+
+  // Return whether the queue is empty.
+  public boolean empty() {
+    return stack1.isEmpty() && stack2.isEmpty();
+  }
+
+}
