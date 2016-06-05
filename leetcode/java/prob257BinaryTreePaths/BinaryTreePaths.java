fee5478871471fe2fc470d69b50bafcf9269076e
AmadouSallah <amadou.saikou.sallah@gmail.com>
2016-06-04 18:10:56 -0700

    Added problem statement

---
+++ leetcode/java/prob257BinaryTreePaths/BinaryTreePaths.java
@@ -0,0 +1,28 @@
+/*
+Binary Tree Paths
+
+Given a binary tree, return all root-to-leaf paths.
+
+For example, given the following binary tree:
+
+       1
+     /   \
+    2     3
+     \
+      5
+All root-to-leaf paths are:
+
+["1->2->5", "1->3"]
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
