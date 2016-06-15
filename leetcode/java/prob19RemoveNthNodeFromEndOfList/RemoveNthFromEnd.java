eab1570ac880eb31365748cf52c437c19a699a1b
AmadouSallah <amadou.saikou.sallah@gmail.com>
2016-06-15 13:12:34 -0700

    Added solution to RemoveNthFromEnd.java

--- leetcode/java/prob19RemoveNthNodeFromEndOfList/RemoveNthFromEnd.java
+++ leetcode/java/prob19RemoveNthNodeFromEndOfList/RemoveNthFromEnd.java
@@ -13,3 +13,64 @@ Note:
 Given n will always be valid.
 Try to do this in one pass.
 */
+
+public class RemoveNthFromEnd {
+
+  public static class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {val = x;}
+  }
+
+  public static ListNode removeNthFromEnd(ListNode head, int n) {
+    ListNode tempNode = new ListNode(0);
+    tempNode.next = head;
+    ListNode firstNode = tempNode, secondNode = tempNode;
+    int i = 0;
+
+    while (i < n) {
+      firstNode = firstNode.next;
+      i++;
+    }
+
+    while (firstNode.next != null) {
+      firstNode = firstNode.next;
+      secondNode = secondNode.next;
+    }
+
+    secondNode.next = secondNode.next.next;
+
+    return tempNode.next;
+  }
+
+  public static void printList(ListNode head) {
+    if (head == null) return;
+    ListNode node = head;
+    while (node != null) {
+      System.out.print(node.val + " -> ");
+      node = node.next;
+    }
+    System.out.println("null");
+  }
+
+  public static void main(String[] args) {
+    ListNode node1 = new ListNode(1);
+    ListNode node2 = new ListNode(2);
+    ListNode node3 = new ListNode(3);
+    ListNode node4 = new ListNode(4);
+    ListNode node5 = new ListNode(5);
+
+    node1.next = node2;
+    node2.next = node3;
+    node3.next = node4;
+    node4.next = node5;
+    node5.next = null;
+
+    System.out.println("Initially, the list is:");
+    printList(node1);
+    removeNthFromEnd(node1, 2);
+    System.out.println("\nAfter calling removeNthFromEnd(node1, 2), the list becomes:");
+    printList(node1);
+  }
+
+}
