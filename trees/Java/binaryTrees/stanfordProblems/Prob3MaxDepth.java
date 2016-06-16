22395c83bd53881b2a99a0819063f8b7973802fd
AmadouSallah <amadou.saikou.sallah@gmail.com>
2016-06-16 13:44:52 -0700

    Added solution for Prob3MaxDepth.java

--- trees/Java/binaryTrees/stanfordProblems/Prob3MaxDepth.java
+++ trees/Java/binaryTrees/stanfordProblems/Prob3MaxDepth.java
@@ -11,3 +11,32 @@ The maxDepth of the empty tree is 0, the maxDepth of the tree below is 3.
    5
 
 */
+
+package stanfordPackage;
+
+public class Prob3MaxDepth {
+
+  public static int maxDepth(Node root) {
+    if (root == null) return 0;
+
+    int left = maxDepth(root.getLeftChild());
+    int right = maxDepth(root.getRightChild());
+
+    return 1 + Math.max(left, right);
+  }
+
+  public static void main(String[] args) {
+    Node node1 = new Node(2);
+    Node node2 = new Node(1);
+    Node node3 = new Node(10);
+    Node node4 = new Node(5);
+
+    node1.setLeftChild(node2);
+    node1.setRightChild(node3);
+    node3.setLeftChild(node4);
+
+    System.out.println(maxDepth(null));
+    System.out.println(maxDepth(node4));
+    System.out.println(maxDepth(node1));
+  }
+}
