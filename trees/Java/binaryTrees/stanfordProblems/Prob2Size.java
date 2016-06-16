51c4d547a219b2fdf4d0218ed87aaf481246818c
AmadouSallah <amadou.saikou.sallah@gmail.com>
2016-06-16 13:06:50 -0700

    Added solution

--- trees/Java/binaryTrees/stanfordProblems/Prob2Size.java
+++ trees/Java/binaryTrees/stanfordProblems/Prob2Size.java
@@ -2,3 +2,14 @@
 This problem demonstrates simple binary tree traversal.
 Given a binary tree, count the number of nodes in the tree.
 */
+
+package stanfordPackage;
+
+public class Prob2Size {
+  public static int size(Node root) {
+
+    if (root == null) return 0;
+
+    return (1 + size(root.getLeftChild()) + size(root.getRightChild()) );
+  }
+}
