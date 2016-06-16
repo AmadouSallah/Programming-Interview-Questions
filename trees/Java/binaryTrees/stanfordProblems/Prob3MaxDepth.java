4a0d900a2ea5828f5abba8de1da60977bd1f75bb
AmadouSallah <amadou.saikou.sallah@gmail.com>
2016-06-16 13:11:52 -0700

    Added problem statement

---
+++ trees/Java/binaryTrees/stanfordProblems/Prob3MaxDepth.java
@@ -0,0 +1,13 @@
+/*
+maxDepth()
+Given a binary tree, compute its "maxDepth" -- the number of nodes along the
+longest path from the root node down to the farthest leaf node.
+The maxDepth of the empty tree is 0, the maxDepth of the tree below is 3.
+
+  2
+ / \
+1   10
+    /
+   5
+
+*/
