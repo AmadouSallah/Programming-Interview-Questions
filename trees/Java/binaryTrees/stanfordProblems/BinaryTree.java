02eea137f1f5c569cc1e82f9693084e8f89b8e5e
AmadouSallah <amadou.saikou.sallah@gmail.com>
2016-06-16 12:54:51 -0700

    Remove Node implementation from BinaryTree.java

--- trees/Java/binaryTrees/stanfordProblems/BinaryTree.java
+++ trees/Java/binaryTrees/stanfordProblems/BinaryTree.java
@@ -61,40 +61,4 @@ public class BinaryTree {
     return node;
   }
 
-  private static class Node {
-    private int data;
-    private Node leftChild, rightChild;
-
-    public Node(int data) {
-      this.data = data;
-      leftChild = null;
-      rightChild = null;
-    }
-
-    public int getData() {
-      return data;
-    }
-
-    public Node getLeftChild() {
-      return leftChild;
-    }
-
-    public Node getRightChild() {
-      return rightChild;
-    }
-
-    public void setData(int data) {
-      this.data = data;
-    }
-
-    public void setLeftChild(Node leftChild) {
-      this.leftChild = leftChild;
-    }
-
-    public void setRightChild(Node rightChild) {
-      this.rightChild = rightChild;
-    }
-  }
-
-
 }
