01c192f19e5841dea3f5d89f2b09e18f301fa36d
AmadouSallah <amadou.saikou.sallah@gmail.com>
2016-06-16 12:38:46 -0700

    Added Prob1Build123.java file

---
+++ trees/Java/binaryTrees/stanfordProblems/Prob1Build123.java
@@ -0,0 +1,14 @@
+/*
+This is a very basic problem with a little pointer manipulation.
+(You can skip this problem if you are already comfortable with pointers.)
+
+Write code that builds the following little 1-2-3 binary search tree...
+  2
+ / \
+1   3
+
+Write the code in three different ways...
+  a: by calling newNode() three times, and using three pointer variables
+  b: by calling newNode() three times, and using only one pointer variable
+  c: by calling insert() three times passing it the root pointer to build up the tree
+*/
