9444e35175fe12c3cf0f857895dda19e86e46d45
AmadouSallah <amadou.saikou.sallah@gmail.com>
2016-06-17 11:14:42 -0700

    Added problem statement

---
+++ trees/Java/binaryTrees/stanfordProblems/Prob4bMaxValue.java
@@ -0,0 +1,6 @@
+/*
+maxValue()
+Given a non-empty binary search tree (an ordered binary tree), return the maximum
+data value found in that tree.
+Note that it is not necessary to search the entire tree.
+*/
