25fd14772f1875864b64a4bc002bd952e777e295
AmadouSallah <amadou.saikou.sallah@gmail.com>
2016-06-17 11:52:23 -0700

    Added solution to Prob6PrintPostorder.java

--- trees/Java/binaryTrees/stanfordProblems/Prob6PrintPostorder.java
+++ trees/Java/binaryTrees/stanfordProblems/Prob6PrintPostorder.java
@@ -16,3 +16,49 @@ This is the sort of bottom-up traversal that would be used, for example, to eval
 an expression tree where a node is an operation like '+' and its subtrees are,
 recursively, the two subexpressions for the '+'.
 */
+
+package treesPackage;
+
+public class Prob6PrintPostorder {
+  public static void printPostorder(Node root) {
+
+    if (root == null) return;
+
+    printPostorder(root.getLeftChild());
+    printPostorder(root.getRightChild());
+    System.out.print(root.getData() + " ");
+
+  }
+
+  public static void main(String[] args) {
+
+    /*
+
+       4
+     /   \
+    2     5
+  /   \
+ 1     3
+
+    */
+    Node node1 = new Node(4);
+    Node node2 = new Node(2);
+    Node node3 = new Node(5);
+    Node node4 = new Node(1);
+    Node node5 = new Node(3);
+
+    node1.setLeftChild(node2);
+    node1.setRightChild(node3);
+    node2.setLeftChild(node4);
+    node2.setRightChild(node5);
+
+    printPostorder(node1);
+    System.out.println();
+
+    printPostorder(node2);
+    System.out.println();
+
+    printPostorder(node3);
+    System.out.println();
+  }
+}
