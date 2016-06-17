958ab30e3d76a8455385d8d488988392193b16c3
AmadouSallah <amadou.saikou.sallah@gmail.com>
2016-06-17 12:08:36 -0700

    Added solution to Prob6bPrintPreorder.java

--- trees/Java/binaryTrees/stanfordProblems/Prob6bPrintPreorder.java
+++ trees/Java/binaryTrees/stanfordProblems/Prob6bPrintPreorder.java
@@ -11,3 +11,49 @@ So the tree...
  1     3
 Produces the output "4 2 1 3 5". The description is complex, but the code is simple.
 */
+
+package treesPackage;
+
+public class Prob6bPrintPreorder {
+  public static void printPreorder(Node root) {
+
+    if (root == null) return;
+
+    System.out.print(root.getData() + " ");
+    printPreorder(root.getLeftChild());
+    printPreorder(root.getRightChild());
+
+  }
+
+  public static void main(String[] args) {
+
+    /*
+
+       4
+     /   \
+    2     5
+  /   \
+ 1     3
+
+    */
+    Node node1 = new Node(4);
+    Node node2 = new Node(2);
+    Node node3 = new Node(5);
+    Node node4 = new Node(1);
+    Node node5 = new Node(3);
+
+    node1.setLeftChild(node2);
+    node1.setRightChild(node3);
+    node2.setLeftChild(node4);
+    node2.setRightChild(node5);
+
+    printPreorder(node1);
+    System.out.println();
+
+    printPreorder(node2);
+    System.out.println();
+
+    printPreorder(node3);
+    System.out.println();
+  }
+}
