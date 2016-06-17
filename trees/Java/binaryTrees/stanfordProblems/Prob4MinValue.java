4715c28bf0a1f18ff32b9310e730975c4cffa000
AmadouSallah <amadou.saikou.sallah@gmail.com>
2016-06-17 11:05:01 -0700

    Added problem statement

---
+++ trees/Java/binaryTrees/stanfordProblems/Prob4MinValue.java
@@ -0,0 +1,8 @@
+/*
+minValue()
+Given a non-empty binary search tree (an ordered binary tree), return the minimum
+data value found in that tree.
+Note that it is not necessary to search the entire tree.
+A maxValue() function is structurally very similar to this function.
+This can be solved with recursion or with a simple while loop.
+*/
