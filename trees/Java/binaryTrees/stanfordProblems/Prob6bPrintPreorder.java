f217b6d74202b82924caa89b55b70833d6e738c6
AmadouSallah <amadou.saikou.sallah@gmail.com>
2016-06-17 11:58:41 -0700

    Added problem statement

---
+++ trees/Java/binaryTrees/stanfordProblems/Prob6bPrintPreorder.java
@@ -0,0 +1,13 @@
+/*
+printPreorder()
+
+Given a binary tree, print out the nodes of the tree in a "postorder" traversal
+So the tree...
+
+       4
+     /   \
+    2     5
+  /   \
+ 1     3
+Produces the output "4 2 1 3 5". The description is complex, but the code is simple.
+*/
