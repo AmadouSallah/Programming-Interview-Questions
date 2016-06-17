3991995400259cde1c85249d5ecea788a1f13809
AmadouSallah <amadou.saikou.sallah@gmail.com>
2016-06-17 11:31:41 -0700

    Added problem statement

---
+++ trees/Java/binaryTrees/stanfordProblems/Prob5PrintInorder.java
@@ -0,0 +1,13 @@
+/*
+ printTree()
+
+Given a binary search tree (aka an "ordered binary tree"), iterate over the nodes
+to print them out in increasing order. So the tree...
+       4
+     /   \
+    2     5
+  /   \
+ 1     3
+Produces the output "1 2 3 4 5". This is known as an "inorder" traversal of the tree.
+Hint: For each node, the strategy is: recur left, print the node data, recur right.
+*/
