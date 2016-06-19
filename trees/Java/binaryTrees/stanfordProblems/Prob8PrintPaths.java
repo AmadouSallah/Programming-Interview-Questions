010ecc82c9cd1cd56ba7bbd566841799d328a220
AmadouSallah <amadou.saikou.sallah@gmail.com>
2016-06-19 12:54:28 -0700

    Added problem statement to Prob8PrintPaths.java

---
+++ trees/Java/binaryTrees/stanfordProblems/Prob8PrintPaths.java
@@ -0,0 +1,19 @@
+/*
+
+printPaths()
+
+Given a binary tree, print out all of its root-to-leaf paths, one per line.
+Example, for the tree below:
+        5
+       / \
+      4   8
+     /   / \
+    11  13  4
+   /  \      \
+  7    2      1
+The root-to-leaf paths are:
+ 5 4 11 7
+ 5 4 11 2
+ 5 8 13
+ 5 8 4 1
+*/
