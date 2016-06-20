5ed15fcb7b91e08248427f76edd7728fdfc68cb1
AmadouSallah <amadou.saikou.sallah@gmail.com>
2016-06-20 14:49:31 -0700

    Added problem statement for Prob9Mirror.java

---
+++ trees/Java/binaryTrees/stanfordProblems/Prob9Mirror.java
@@ -0,0 +1,19 @@
+/*
+Write a Function to Convert a Binary Tree into its Mirror Tree
+
+Definition: Mirror of a Binary Tree T is another Binary Tree M(T)
+with left and right children of all non-leaf nodes interchanged.
+
+Example: the tree below
+     4
+    / \
+   2   5
+  / \
+ 1   3
+is changed to
+     4
+    / \
+   5   2
+  / \
+ 3   1
+*/
