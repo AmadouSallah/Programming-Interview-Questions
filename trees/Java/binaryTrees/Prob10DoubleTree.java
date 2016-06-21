d0d421b5cfa11bcfff6e193ded5e9b7ca27810f7
AmadouSallah <amadou.saikou.sallah@gmail.com>
2016-06-21 14:23:22 -0700

    Added solution to Prob10DoubleTree.java

--- trees/Java/binaryTrees/Prob10DoubleTree.java
+++ trees/Java/binaryTrees/Prob10DoubleTree.java
@@ -23,3 +23,48 @@ The resulting tree should still be a binary search tree.
 
 */
 
+
+package treesPackage;
+
+public class Prob10DoubleTree {
+  public static void doubleTree(Node root) {
+    if (root == null) return;
+
+    doubleTree(root.getLeftChild());
+    doubleTree(root.getRightChild());
+
+    Node oldLeftNode = root.getLeftChild();
+    root.setLeftChild(new Node(root.getData()));
+    root.getLeftChild().setLeftChild(oldLeftNode);
+  }
+
+  public static void printInorder(Node root) {
+    if (root == null) return;
+    printInorder(root.getLeftChild());
+    System.out.print(root.getData() + " ");
+    printInorder(root.getRightChild());
+  }
+
+  public static void main(String[] args) {
+        /*
+         2
+        / \
+       1   3
+        */
+    Node node1 = new Node(2);
+    Node node2 = new Node(1);
+    Node node3 = new Node(3);
+
+    node1.setLeftChild(node2);
+    node1.setRightChild(node3);
+
+    System.out.println("Initially, the tree printed inorder is ");
+    printInorder(node1);
+    System.out.println();
+
+    doubleTree(node1);
+    System.out.println("\nAfter calling doubleTree(root), the tree becomes:");
+    printInorder(node1);
+    System.out.println();
+  }
+}
