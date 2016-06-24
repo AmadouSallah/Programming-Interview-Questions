2b41dcf4252f93dc427727f1cc9688ec49e0e0a2
AmadouSallah <amadou.saikou.sallah@gmail.com>
2016-06-24 05:16:00 -0700

    Added solution for MaxArea.java

--- leetcode/java/prob11ContainerWithMostWater/MaxArea.java
+++ leetcode/java/prob11ContainerWithMostWater/MaxArea.java
@@ -8,3 +8,25 @@ Find two lines, which together with x-axis forms a container, such that the cont
 Note: You may not slant the container.
 
  */
+
+public class MaxArea {
+  public static int maxArea(int[] height) {
+    int left = 0, right = height.length-1, maxArea = 0;
+    while (left < right) {
+      int leftHeight = height[left], rightHeight = height[right];
+      int currentArea = (Math.min(leftHeight, rightHeight)) * (right - left);
+      maxArea = Math.max(maxArea, currentArea);
+
+      if (leftHeight < rightHeight) { // we might find a height to the right of left that is greater than it.
+        left++;                     // so we increment left
+      } else {                        // right <= left, so we decrement right since there might be a height
+        right--;                     // to its left that is larger.
+      }
+    }
+    return maxArea;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(maxArea(new int[] {1,1}));
+  }
+}
