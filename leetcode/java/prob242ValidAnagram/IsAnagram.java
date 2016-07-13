631e0e629696b0401201a599773a77514b2e5a2d
AmadouSallah <amadou.saikou.sallah@gmail.com>
2016-07-13 13:11:35 -0700

    Added solution to IsAnagram.java

--- leetcode/java/prob242ValidAnagram/IsAnagram.java
+++ leetcode/java/prob242ValidAnagram/IsAnagram.java
@@ -12,3 +12,37 @@ You may assume the string contains only lowercase alphabets.
 Follow up:
 What if the inputs contain unicode characters? How would you adapt your solution to such case?
 */
+
+import java.util.Arrays;
+
+public class IsAnagram {
+  public static boolean isAnagram(String s, String t) {
+    int sLen = s.length(), tLen = t.length();
+    if (sLen != tLen) return false;
+    if (sLen == 0 && tLen == 0) return true;
+    if (sLen == 0 || tLen == 0) return false; // this means that only one of them is 0
+
+    char[] sArray = s.toCharArray();
+    char[] tArray = t.toCharArray();
+
+    Arrays.sort(sArray);
+    Arrays.sort(tArray);
+
+    for (int i = 0; i < sLen; i++) {
+      if (sArray[i] != tArray[i])
+        return false;
+    }
+
+    return true;
+  }
+
+  public static void main(String[] args) {
+    System.out.println("isAnagram(\"\", \"\") = " + isAnagram("", ""));
+    System.out.println("isAnagram(\"a\", \"\") = " + isAnagram("a", ""));
+    System.out.println("isAnagram(\"\", \"b\") = " + isAnagram("", "b"));
+    System.out.println("isAnagram(\"a\", \"b\") = " + isAnagram("a", "b"));
+    System.out.println("isAnagram(\"anagram\", \"nagaram\") = " + isAnagram("anagram", "nagaram"));
+    System.out.println("isAnagram(\"rat\", \"car\") = " + isAnagram("rat", "car"));
+    System.out.println("isAnagram(\"hello\", \"hi\") = " + isAnagram("hello", "hi"));
+    }
+}
