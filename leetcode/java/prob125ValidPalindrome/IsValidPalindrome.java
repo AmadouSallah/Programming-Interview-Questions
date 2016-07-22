6b0c01bd01e45a13362c6866db2f5002ffbfa237
AmadouSallah <amadou.saikou.sallah@gmail.com>
2016-07-22 00:33:41 -0700

    Added solution to IsValidPalindrome.java

--- leetcode/java/prob125ValidPalindrome/IsValidPalindrome.java
+++ leetcode/java/prob125ValidPalindrome/IsValidPalindrome.java
@@ -12,3 +12,32 @@ Have you consider that the string might be empty? This is a good question to ask
 
 For the purpose of this problem, we define empty string as valid palindrome.
 */
+
+public class IsValidPalindrome {
+  public static boolean isValidPalindrome(String s) {
+
+    int l = 0, r = s.length()-1;
+
+    while (l < r) {
+
+      while (l < r && !Character.isLetterOrDigit(s.charAt(l)))
+        l++;
+
+      while (l < r && !Character.isLetterOrDigit(s.charAt(r)))
+        r--;
+
+      if (Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r)))
+        return false;
+
+        l++;
+        r--;
+    }
+    return true;
+  }
+
+  public static void main(String[] args) {
+    System.out.println("isValidPalindrome(\"\"): " + isValidPalindrome(""));
+    System.out.println("isValidPalindrome(\"A man, a plan, a canal: Panama\"): " + isValidPalindrome("A man, a plan, a canal: Panama"));
+    System.out.println("isValidPalindrome(\"race a car\"): " + isValidPalindrome("race a car"));
+  }
+}
