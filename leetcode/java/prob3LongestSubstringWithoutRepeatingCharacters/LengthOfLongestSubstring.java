9e51ee8eaae6cac9b44efcbd53a58ee4299ac971
AmadouSallah <amadousallah13@gmail.com>
2016-08-05 02:20:37 -0700

    Added 2nd solution with O(n) running time and O(1) space complexities

--- leetcode/java/prob3LongestSubstringWithoutRepeatingCharacters/LengthOfLongestSubstring.java
+++ leetcode/java/prob3LongestSubstringWithoutRepeatingCharacters/LengthOfLongestSubstring.java
@@ -5,9 +5,11 @@ For example, the longest substring without repeating letters for "abcabcbb" is "
 whith the length of 3. For "bbbbb" the longest substring is "b", with the length of 1.
 */
 import java.util.HashSet;
+import java.util.Arrays;
 
 public class LengthOfLongestSubstring {
 
+  // O(n) running time and O(n) space complexities
   public static int lengthOfLongestSubstring(String s) {
     if (s == null || s.length() == 0) return 0;
 
@@ -31,18 +33,73 @@ public class LengthOfLongestSubstring {
     return Math.max(maxLen, i-j);
   }
 
+  // Solution 2: O(n) running time and O(1) space complexities
+  /*
+
+  We initialize i and maxLen to 0. i will point to the index of the beginning of the
+  current substring without repeating characters; maxLen will track the longest
+  lenth so far as we traverse the input string.
+
+  We also define a  mapping of the characters to their index. Note that there are 256 ASCII characters,
+  so we define an array, charMap, of size 256 and initialize each of its elements to -1.
+
+  For each character c of the input string s, charMap[c] (same as charMap[(int) c]) will be set to the
+  index of c in s. Note that (int) c is the corresponding integer ASCII value of c.
+  If c is repeated somewhere in s, charMap[c] will, at the end, hold the index of the last position of c in s.
+
+  Example, if s = "abdae", since the ASCII value of 'a' is 97, and the 1st 'a' is at position 0 in "abdae",
+  charMap[97] will intially be set to 0. But eventually, when we reach the 2nd 'a' at position 3, charMap[97]
+  will be set to 3.
+  Also, since the ASCII value of 'd' is 100 and 'd' is at position 2 in "abdae", charMap[100] will be set to 2.
+  Similarly, charMap[98] will be set to 1 (for 'b'), and charMap[101] will be set to 4 (for 'e').
+
+  We loop through the characters of input string s.
+    Remember i points to the beginning of the current substring in consideration.
+
+    If the current character has already been seen, that is if it is the same as the character that i points to,
+    we make i point to the next character since we don't want repeating characters.
+    Remeber that charMap[c] represents the position of character c in s. Therefore, setting i to
+    charMap[c] + 1 will make i point to the next character in s.
+
+    We then set the value of charMap[c] to the current index. We also update maxLen to the number of characters from
+    i to the current character it this is greater than the previous maxLen.
+  */
+  public static int lengthOfLongestSubstring2(String s) {
+
+    if (s == null || s.length() == 0) return 0;
+
+    int i = 0, maxLen = 0, n = s.length();
+    int[] charMap = new int[256];
+    Arrays.fill(charMap, -1);
+
+    for (int j = 0; j < n; j++) {
+      if (charMap[s.charAt(j)] >= i) // if the current character has already been seen
+        i = charMap[s.charAt(j)] + 1; // we point i to the next character in s
+
+      charMap[s.charAt(j)] = j;
+      maxLen = Math.max(maxLen, j-i+1);
+    }
+    return maxLen;
+  }
+
   public static void main(String[] args) {
-    String s0 = null;
-    String s1 = "";
-    String s2 = "a";
-    String s3 = "abcabcbb";
-    String s4 = "bbbbb";
-    String s5 = "ab";
-    System.out.println(lengthOfLongestSubstring(s0)); // 0
-    System.out.println(lengthOfLongestSubstring(s1)); // 0
-    System.out.println(lengthOfLongestSubstring(s2)); // 1
-    System.out.println(lengthOfLongestSubstring(s3)); // 3
-    System.out.println(lengthOfLongestSubstring(s4)); // 1
-    System.out.println(lengthOfLongestSubstring(s5)); // 2
+
+    System.out.println("Testing solution with O(n) runtime and O(n) space:");
+    System.out.println("lengthOfLongestSubstring(\"null\") = " + lengthOfLongestSubstring(null)); // 0
+    System.out.println("lengthOfLongestSubstring(\"\") = " + lengthOfLongestSubstring("")); // 0
+    System.out.println("lengthOfLongestSubstring(\"a\") = " + lengthOfLongestSubstring("a")); // 1
+    System.out.println("lengthOfLongestSubstring(\"abcabcbb\") = " + lengthOfLongestSubstring("abcabcbb")); // 3
+    System.out.println("lengthOfLongestSubstring(\"bbbbb\") = " + lengthOfLongestSubstring("bbbbb")); // 1
+    System.out.println("lengthOfLongestSubstring(\"ab\") = " + lengthOfLongestSubstring("ab")); // 2
+    System.out.println("lengthOfLongestSubstring(\"pwwkew\") = " + lengthOfLongestSubstring2("pwwkew")); // 3
+
+    System.out.println("\nTesting solution with O(n) runtime and O(1) space:");
+    System.out.println("lengthOfLongestSubstring2(\"null\") = " + lengthOfLongestSubstring2(null)); // 0
+    System.out.println("lengthOfLongestSubstring2(\"\") = " + lengthOfLongestSubstring2("")); // 0
+    System.out.println("lengthOfLongestSubstring2(\"a\") = " + lengthOfLongestSubstring2("a")); // 1
+    System.out.println("lengthOfLongestSubstring2(\"abcabcbb\") = " + lengthOfLongestSubstring2("abcabcbb")); // 3
+    System.out.println("lengthOfLongestSubstring2(\"bbbbb\") = " + lengthOfLongestSubstring2("bbbbb")); // 1
+    System.out.println("lengthOfLongestSubstring2(\"ab\") = " + lengthOfLongestSubstring2("ab")); // 2
+    System.out.println("lengthOfLongestSubstring2(\"pwwkew\") = " + lengthOfLongestSubstring2("pwwkew")); // 3
   }
 }
