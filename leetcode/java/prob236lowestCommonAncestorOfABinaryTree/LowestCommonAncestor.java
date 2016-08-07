97e861a9ae3ed25ced074ff010c2f1c2625f44b5
AmadouSallah <amadousallah13@gmail.com>
2016-08-07 14:21:12 -0700

    Added solution to LowestCommonAncestor.java

--- leetcode/java/prob236lowestCommonAncestorOfABinaryTree/LowestCommonAncestor.java
+++ leetcode/java/prob236lowestCommonAncestorOfABinaryTree/LowestCommonAncestor.java
@@ -18,3 +18,30 @@ For example, the lowest common ancestor (LCA) of nodes 5 and 1 is 3.
 Another example is LCA of nodes 5 and 4 is 5, since a node can be a descendant of
 itself according to the LCA definition.
 */
+
+public class LowestCommonAncestor {
+
+  public class TreeNode {
+    int val;
+    TreeNode left, right;
+    TreeNode(int x) {
+      val = x;
+    }
+  }
+
+  public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+    if (root == null || root == p || root == q)
+      return root;
+
+
+    TreeNode left = lowestCommonAncestor(root.left, p, q);
+    TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+    if (left != null && right != null)
+      return root;
+
+    return (left == null) ? right : left;
+
+  }
+}
