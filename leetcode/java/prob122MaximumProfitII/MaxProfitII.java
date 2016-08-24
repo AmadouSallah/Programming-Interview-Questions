4baae0347b48e3353691f38f5e89686c08e4c5d4
AmadouSallah <amadousallah13@gmail.com>
2016-08-24 02:31:43 -0700

    Added solution for MaxProfitII.java

--- leetcode/java/prob122MaximumProfitII/MaxProfitII.java
+++ leetcode/java/prob122MaximumProfitII/MaxProfitII.java
@@ -9,3 +9,24 @@ you may not engage in multiple transactions at the same time
 (ie, you must sell the stock before you buy again).
 
 */
+
+public class MaxProfitII {
+
+  public static int maxProfit(int[] prices) {
+
+    int max = 0, diff, n = prices.length;
+
+    for (int i = 1; i < n; i++) {
+      diff = prices[i] - prices[i-1];
+      if (diff > 0) max += diff;
+    }
+
+    return max;
+  }
+
+  public static void main(String[] args) {
+    System.out.println("maxProfit(new int[] {}) = " + maxProfit(new int[] {})); // 0
+    System.out.println("maxProfit(new int[] {7, 1, 5, 3, 6, 4}) = " + maxProfit(new int[] {7, 1, 5, 3, 6, 4})); // (5-1) + (6-3) = 7
+    System.out.println("maxProfit(new int[] {7, 6, 4, 3, 1}) = " + maxProfit(new int[] {7, 6, 4, 3, 1})); // 0
+  }
+}
