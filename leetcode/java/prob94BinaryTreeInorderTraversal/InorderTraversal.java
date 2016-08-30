38c60ccacfd6f3f67c05849192d7b63bf103d953
AmadouSallah <amadousallah13@gmail.com>
2016-08-29 23:58:23 -0700

    Added Recursive solution to InorderTraversal.java

--- leetcode/java/prob94BinaryTreeInorderTraversal/InorderTraversal.java
+++ leetcode/java/prob94BinaryTreeInorderTraversal/InorderTraversal.java
@@ -14,3 +14,36 @@ return [1,3,2].
 
 Note: Recursive solution is trivial, could you do it iteratively?
 */
+
+import java.util.List;
+import java.util.ArrayList;
+
+/* RECURSIVE SOLUTION */
+public class InorderTraversal {
+
+  public class TreeNode {
+    int val;
+    TreeNode left, right;
+    TreeNode(int x) {val = x;}
+  }
+
+  public static List<Integer> inorderTraversal(TreeNode root) {
+    List<Integer> list = new ArrayList<Integer>();
+    helper(root, list);
+    return list;
+  }
+
+  public static void helper(TreeNode root, List<Integer> list) {
+    if (root != null) {
+
+      if (root.left != null)
+        helper(root.left, list);
+
+      list.add(root.val);
+
+      if (root.right != null)
+        helper(root.right, list);
+    }
+  }
+
+}
