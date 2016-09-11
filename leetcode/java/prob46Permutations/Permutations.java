3a6a937acea3821852442071e7fb618b3b19f293
AmadouSallah <amadousallah13@gmail.com>
2016-09-11 12:16:13 -0700

    Added solution for Permutations.java

--- leetcode/java/prob46Permutations/Permutations.java
+++ leetcode/java/prob46Permutations/Permutations.java
@@ -14,3 +14,46 @@ For example,
   [3,2,1]
 ]
 */
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Permutations {
+
+  public static List<List<Integer>> permutations(int[] nums) {
+    List<List<Integer>> result = new ArrayList<>();
+
+    if (nums == null || nums.length == 0) return result;
+
+    List<Integer> tempList = new ArrayList<Integer>();
+
+    permute(nums, result, tempList);
+
+    return result;
+  }
+
+  public static void permute(int[] nums, List<List<Integer>> result, List<Integer> tempList) {
+
+    if (tempList.size() == nums.length) {
+      result.add(new ArrayList<>(tempList));
+      return;
+    }
+
+    for (int i = 0; i < nums.length; i++) {
+      int currentElement = nums[i];
+      if (!tempList.contains(currentElement)) {
+        tempList.add(currentElement);
+        permute(nums, result, tempList);
+        tempList.remove(tempList.size()-1);
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    System.out.println("permutations(null) = " + permutations(null));
+    System.out.println("permutations(new int[] {}) = " + permutations(new int[] {}));
+    System.out.println("permutations(new int[] {1}) = " + permutations(new int[] {1}));
+    System.out.println("permutations(new int[] {1, 2}) = " + permutations(new int[] {1, 2}));
+    System.out.println("permutations(new int[] {1, 2, 3}) = " + permutations(new int[] {1, 2, 3}));
+  }
+}
