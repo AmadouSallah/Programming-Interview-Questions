8248e3bb3a81595e076ef64e63bef5570f6b240f
AmadouSallah <amadousallah13@gmail.com>
2016-09-11 13:08:32 -0700

    Added problem statement for PermuteUnique.java

---
+++ leetcode/java/prob47PermutationsII/PermuteUnique.java
@@ -0,0 +1,14 @@
+/*
+Problem 47: Permutations II
+
+Given a collection of numbers that might contain duplicates, return all possible unique permutations.
+
+For example,
+[1,1,2] have the following unique permutations:
+[
+  [1,1,2],
+  [1,2,1],
+  [2,1,1]
+]
+
+*/
