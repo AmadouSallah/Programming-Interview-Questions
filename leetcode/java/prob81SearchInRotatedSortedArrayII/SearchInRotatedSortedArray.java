6d6296005535ecb20b15959879c76dccb14cb48b
AmadouSallah <amadousallah13@gmail.com>
2016-09-23 12:30:51 -0700

    Added problem statement for SearchInRotatedSortedArray.java

---
+++ leetcode/java/prob81SearchInRotatedSortedArrayII/SearchInRotatedSortedArray.java
@@ -0,0 +1,10 @@
+/*
+Problem 81. Search in Rotated Sorted Array II
+
+Follow up for "Search in Rotated Sorted Array":
+What if duplicates are allowed?
+
+Would this affect the run-time complexity? How and why?
+
+Write a function to determine if a given target is in the array.
+*/
