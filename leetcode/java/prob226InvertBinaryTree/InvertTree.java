5bccb5e3d9bc07efc8257f8ece4090e983c05e09
AmadouSallah <amadousallah13@gmail.com>
2016-10-18 20:53:26 -0700

    Added iterative solution to InvertTree.java

--- leetcode/java/prob226InvertBinaryTree/InvertTree.java
+++ leetcode/java/prob226InvertBinaryTree/InvertTree.java
@@ -19,6 +19,9 @@ to
 
 */
 
+import java.util.Queue;
+import java.util.LinkedList;
+
 public class InvertTree {
 
   public class TreeNode {
@@ -41,4 +44,28 @@ public class InvertTree {
 
     return root;
   }
+
+  // Iterative solution: O(n) runtime and O(n) space
+  public static TreeNode invertTree2(TreeNode root) {
+
+    if (root == null) return root;
+
+    Queue<TreeNode> queue = new LinkedList<TreeNode>();
+    queue.add(root);
+
+    while (!queue.isEmpty()) {
+
+      TreeNode current = queue.poll();
+      TreeNode temp = current.left;
+      current.left = current.right;
+      current.right = temp;
+
+      if (current.left != null) queue.add(current.left);
+      if (current.right != null) queue.add(current.right);
+    }
+
+    return root;
+  }
+
 }
+
