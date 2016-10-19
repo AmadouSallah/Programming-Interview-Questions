64912e91d441163c4583e96f245016e0beb00b57
AmadouSallah <amadousallah13@gmail.com>
2016-10-18 20:39:42 -0700

    Added problem statement for InvertTree.java

---
+++ leetcode/java/prob226InvertBinaryTree/InvertTree.java
@@ -0,0 +1,20 @@
+/*
+
+Leetcode Problem 226. Invert Binary Tree
+
+Invert a binary tree.
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+
+to
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+
+*/
