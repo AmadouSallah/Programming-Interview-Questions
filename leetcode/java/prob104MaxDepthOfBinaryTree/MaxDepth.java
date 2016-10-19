3f2778aa8236b4e370d4e2b1984cf96ddd898571
AmadouSallah <amadousallah13@gmail.com>
2016-10-18 18:23:40 -0700

    Added solution for MaxDepth.java

--- leetcode/java/prob104MaxDepthOfBinaryTree/MaxDepth.java
+++ leetcode/java/prob104MaxDepthOfBinaryTree/MaxDepth.java
@@ -8,3 +8,25 @@ The maximum depth is the number of nodes along the longest path from the root no
 down to the farthest leaf node.
 
 */
+
+public class MaxDepth {
+
+  public class TreeNode {
+    int val;
+    TreeNode left, right;
+
+    TreeNode(int x) {
+      val = x;
+    }
+  }
+
+  public int maxDepth(TreeNode root) {
+
+    if (root == null) return 0;
+
+    int left = maxDepth(root.left);
+    int right = maxDepth(root.right);
+
+    return 1 + Math.max(left, right);
+  }
+}
