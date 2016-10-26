5dd2d2d1811fb791919b9012cc6df8459c4f5909
AmadouSallah <amadousallah13@gmail.com>
2016-10-25 22:55:58 -0700

    Added solution for PathSumII.java

--- leetcode/java/prob113PathSumII/PathSumII.java
+++ leetcode/java/prob113PathSumII/PathSumII.java
@@ -16,3 +16,46 @@ return
 [[5,4,11,2], [5,8,4,5]]
 
 */
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class PathSumII {
+
+  public class TreeNode {
+    int val;
+    TreeNode left, right;
+
+    TreeNode(int x) {
+      val = x;
+    }
+  }
+
+  public static List<List<Integer>> pathSum(TreeNode root, int sum) {
+
+    List<List<Integer>> result = new ArrayList<>();
+
+    List<Integer> tempList = new ArrayList<>();
+
+    pathSumHelper(root, sum, result, tempList);
+
+    return result;
+  }
+
+    public static void pathSumHelper(TreeNode root, int sum, List<List<Integer>> result, List<Integer> tempList) {
+
+      if (root == null) return;
+
+      tempList.add(root.val);
+      int newSum = sum - root.val;
+
+      if (newSum == 0 && root.left == null && root.right == null) {
+          result.add(new ArrayList<>(tempList));
+          return;
+      }
+
+      pathSumHelper(root.left, newSum, result, new ArrayList<>(tempList));
+      pathSumHelper(root.right, newSum, result, new ArrayList<>(tempList));
+    }
+
+}
