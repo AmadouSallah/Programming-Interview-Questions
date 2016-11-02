814de452014fc367615556943d8a7ede55e05404
AmadouSallah <amadousallah13@gmail.com>
2016-11-01 20:45:12 -0700

    Added solution to LevelOrder.java

--- leetcode/java/prob102BinaryTreeLevelOrderTraversal/LevelOrder.java
+++ leetcode/java/prob102BinaryTreeLevelOrderTraversal/LevelOrder.java
@@ -15,3 +15,44 @@ return its level order traversal as:
 [[3], [9,20], [15,7] ]
 
 */
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class LevelOrder {
+
+  public class TreeNode {
+    int val;
+    TreeNode left, right;
+    TreeNode(int x) { val = x; }
+  }
+
+  public static List<List<Integer>> levelOrder(TreeNode root) {
+
+    List<List<Integer>> result = new ArrayList<List<Integer>>();
+
+    if (root == null) return result;
+
+    Queue<TreeNode> queue = new LinkedList<TreeNode>();
+    queue.offer(root);
+
+    while (!queue.isEmpty()) {
+
+      List<Integer> list = new ArrayList<Integer>();
+      int level = queue.size();
+
+      for (int i = 0; i < level; i++) {
+        TreeNode node = queue.poll();  // queue.remove() would work too
+        list.add(node.val);
+
+        if (node.left != null) queue.offer(node.left); // queue.add(node.left) would also work
+        if (node.right != null) queue.offer(node.right); // Or queue.add(node.right)
+      }
+
+      result.add(list);
+    }
+    return result;
+  }
+}
