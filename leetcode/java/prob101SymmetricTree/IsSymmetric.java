4ddb964ce29d7ac84ab06e630228d24ecf96adb4
AmadouSallah <amadousallah13@gmail.com>
2016-11-22 13:02:28 -0800

    Added recursive solution: O(n) runtime and O(n) space complexities

--- leetcode/java/prob101SymmetricTree/IsSymmetric.java
+++ leetcode/java/prob101SymmetricTree/IsSymmetric.java
@@ -21,3 +21,29 @@ But the following [1,2,2,null,3,null,3] is not:
 Note: Bonus points if you could solve it both recursively and iteratively.
 
 */
+
+public class IsSymmetric {
+
+  private static class TreeNode {
+   int val;
+   TreeNode left, right;
+   TreeNode(int x) { val = x; }
+  }
+
+    /* Recrusive solution: O(n) run time and O(n) space (in worst case, tree is
+      linear => height = n => O(n) space due to stack) */
+
+    public static boolean isSymmetric(TreeNode root) {
+        return areMirrors(root, root);
+    }
+
+    public static boolean areMirrors(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) {
+            return true;
+        } else if (t1 == null || t2 == null) {
+            return false;
+        } else {
+            return ( (t1.val == t2.val) && areMirrors(t1.left, t2.right) && areMirrors(t1.right, t2.left) );
+        }
+    }
+}
