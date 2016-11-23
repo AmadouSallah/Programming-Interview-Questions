bc1a5d4143ff756c49165488d827dde881a7a29d
AmadouSallah <amadousallah13@gmail.com>
2016-11-23 15:09:26 -0800

    Added solution to DeleteDuplicates.java

--- leetcode/java/prob83RemoveDuplicatesFromSortedList/DeleteDuplicates.java
+++ leetcode/java/prob83RemoveDuplicatesFromSortedList/DeleteDuplicates.java
@@ -8,3 +8,28 @@ Given 1->1->2, return 1->2.
 Given 1->1->2->3->3, return 1->2->3.
 
 */
+
+public class DeleteDuplicates {
+
+  public static class ListNode {
+    int val;
+    ListNode next;
+    public ListNode(int val) {
+      this.val = val;
+    }
+  }
+
+  public static ListNode deleteDuplicates(ListNode head) {
+    if (head == null || head.next == null)
+      return head;
+
+    ListNode node = head;
+    while (node != null && node.next != null) {
+      if (node.val == node.next.val)
+        node.next = node.next.next;
+      else
+        node = node.next;
+    }
+    return head;
+  }
+}
