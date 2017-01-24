359b695f5e31ef178759d24914d6d93fb75b8d8b
AmadouSallah <amadousallah13@gmail.com>
2017-01-23 16:50:34 -0800

    Added solution for MovingAverage.java

--- leetcode/java/prob346MovingAverageFromDataStream/MovingAverage.java
+++ leetcode/java/prob346MovingAverageFromDataStream/MovingAverage.java
@@ -11,3 +11,26 @@ m.next(10) = (1 + 10) / 2
 m.next(3) = (1 + 10 + 3) / 3
 m.next(5) = (10 + 3 + 5) / 3
 */
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class MovingAverage {
+  private int size, count, sum;
+  Queue<Integer> queue = new LinkedList<Integer>();
+
+  public MovingAverage(int size) {
+    this.size = size;
+  }
+
+  public double next(int val) {
+    if (count == size)
+      sum -= queue.poll();
+    else
+      count++;
+
+    sum += val;
+    queue.add(val);
+
+    return ((double) sum) / ((double) count);
+  }
+}
