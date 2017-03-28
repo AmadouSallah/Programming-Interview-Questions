d9178e1da3507c6e7d837a32b7a9893e1255a935
AmadouSallah <amadousallah13@gmail.com>
2017-03-27 20:16:36 -0700

    Added problem statement to AnagramPalindrome.java

---
+++ AnagramPalindrome.java
@@ -0,0 +1,11 @@
+/*
+Given an input string s, write a program to check if any of its anagrams is a palindrome.
+The program returns true if at least one of its anagrams is a palindrome, and false otherwise.
+
+Example
+for input s = "carrace", 2 of its anagrams, "racecar" and "carerac", are both palindromes => true
+For s = "aac", an anagram of s is "aca", which is a palindrome => true
+For s = "abc", none of its anagrams is a palindrome => false
+s = "icvic", anagram "civic" is a palindrome, so return true
+s = "civil", none of its anagrams is a palindrome, so return false
+*/
