92d1c9ff7e00dcf8b69f5d97a787cecc57d87c35
AmadouSallah <amadousallah13@gmail.com>
2017-03-27 20:39:48 -0700

    Added solution for AnagramPalindrome.java

--- AnagramPalindrome.java
+++ AnagramPalindrome.java
@@ -9,3 +9,37 @@ For s = "abc", none of its anagrams is a palindrome => false
 s = "icvic", anagram "civic" is a palindrome, so return true
 s = "civil", none of its anagrams is a palindrome, so return false
 */
+
+import java.util.Set;
+import java.util.HashSet;
+
+public class AnagramPalindrome {
+
+  public static boolean anagramPalindrome(String s) {
+
+    if (s == null || s.length() <= 1) return true;
+    
+    Set<Character> set = new HashSet<Character>();
+    int n = s.length();
+
+    for (int i = 0; i < n; i++) {
+      char c = s.charAt(i);
+      // if c is in the set, we remove it; otherwise, we add it to the set
+      if (set.contains(c)) {
+        set.remove(c);
+      } else {
+        set.add(c);
+      }
+    }
+    // The set should contains at most 1 character in other for any of its anagrams to be a palindrome.
+    return set.size() <= 1;
+
+  }
+  public static void main(String[] args) {
+    System.out.println("anagramPalindrome(\"carrace\") = " + anagramPalindrome("carrace"));
+    System.out.println("anagramPalindrome(\"civic\") = " + anagramPalindrome("civic"));
+    System.out.println("anagramPalindrome(\"civil\") = " + anagramPalindrome("civil"));
+    System.out.println("anagramPalindrome(\"baa\") = " + anagramPalindrome("baa"));
+    System.out.println("anagramPalindrome(\"bac\") = " + anagramPalindrome("bac"));
+  }
+}
