c9365606559558c6fba2207581665520499e9d08
AmadouSallah <amadousallah13@gmail.com>
2017-04-10 15:18:11 -0700

    Added recursive solution for ReverseLinkedList.java

--- leetcode/java/prob206ReverseLinkedList/ReverseLinkedList.java
+++ leetcode/java/prob206ReverseLinkedList/ReverseLinkedList.java
@@ -3,3 +3,26 @@ Problem 206. Reverse Linked List
 
 Reverse a singly linked list.
 */
+
+public class ReverseLinkedList {
+
+  public static class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode (int x) {
+      this.val = x;
+    }
+  }
+
+  // Recursive solution: O(n) runtime and O(n) space complexities
+  public static ListNode reverseList(ListNode head) {
+    if (head == null || head.next == null) return head;
+
+    ListNode second = reverseList(head.next);
+    head.next.next = head;
+    head.next = null;
+
+    return head;
+  }
+}
