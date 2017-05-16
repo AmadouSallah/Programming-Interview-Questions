f5a6acc7bf6f9d161d3043364bd06eb222659e8e
AmadouSallah <amadousallah13@gmail.com>
2017-05-15 22:57:47 -0700

    Added solution for CoinChange.java

--- leetcode/java/prob322CoinChange/CoinChange.java
+++ leetcode/java/prob322CoinChange/CoinChange.java
@@ -17,3 +17,34 @@ Note:
 You may assume that you have an infinite number of each kind of coin.
 
 */
+
+import java.util.Arrays;
+
+public class CoinChange {
+  // O(nm), where n = coins.length and m = amount
+  public static int coinChange(int[] coins, int amount) {
+    int max = amount + 1;
+    int[] dp = new int[amount + 1];
+    Arrays.fill(dp, max);
+    dp[0] = 0; // it takes 0 number of coins to form an amount of 0
+    for (int i = 1; i <= amount; i++) { // i represpents current amount
+      for (int j = 0; j < coins.length; j++) { // j represpents the coin index
+        int currentCoin = coins[j];
+        int remainingAmount = i - currentCoin;
+        if (remainingAmount >= 0) {
+          // we take the minimum between choosing the current coin (1 + dp[remainingAmount])
+          // and not choosing the current coin (dp[i])
+          dp[i] = Math.min(dp[i], 1 + dp[remainingAmount]);
+        }
+      }
+    }
+    return (dp[amount] > amount) ? -1 : dp[amount];
+  }
+
+  public static void main(String[] args) {
+    int[] coins1 = {1,2,5};
+    int[] coins2 = {2};
+    System.out.println("coinChange([1, 2, 5], 11) = " + coinChange(coins1, 11));
+    System.out.println("coinChange([2], 3) = " + coinChange(coins2, 3));
+  }
+}
