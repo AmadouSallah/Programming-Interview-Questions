5a14505407215880410d768457b344ec601c6a58
AmadouSallah <amadousallah13@gmail.com>
2017-05-20 14:23:09 -0700

    Added solution for AlienOrder.java

--- leetcode/java/prob269AlienDictionary/AlienOrder.java
+++ leetcode/java/prob269AlienDictionary/AlienOrder.java
@@ -29,3 +29,82 @@ You may assume that if a is a prefix of b, then a must appear before b in the gi
 If the order is invalid, return an empty string.
 There may be multiple valid order of letters, return any one of them is fine.
 */
+
+import java.util.*;
+
+public class AlienOrder {
+
+  public static String alienOrder(String[] words) {
+
+    String result = "";
+    if (words == null || words.length == 0) return result;
+
+    Map<Character, Integer> indegreeMap = new HashMap<>();
+    Map<Character, Set<Character>> map = new HashMap<>();
+    Queue<Character> queue = new LinkedList<>();
+
+    // Let's put each character in indegreeMap with value of 0
+    for (String word : words) {
+      for (int i = 0; i < word.length(); i++) {
+        char c = word.charAt(i);
+        indegreeMap.put(c, 0);
+      }
+    }
+
+    /*
+    We compare 2 adjacent words at a time. We look for the 1st 2 characters that differ in these 2 words,
+    say c1 in the first word and c2 in the 2nd.
+    Then for key c1 in map, we add c2 (if not already present) to the set that correspond to the value of c1
+    We then do the same for the next 2 adjacent words until the end of the input array. 
+    */
+    for (int i = 0; i < words.length-1; i++) {
+      String currentWord = words[i], nextWord = words[i+1];
+      int minLen = Math.min(currentWord.length(), nextWord.length());
+      for (int j = 0; j < minLen; j++) {
+        char c1 = currentWord.charAt(j), c2 = nextWord.charAt(j);
+        if (c1 != c2) { // then c1 comes before c2 in the dictionary
+          Set<Character> set = new HashSet<>();
+          if (map.containsKey(c1)) set = map.get(c1);
+          if (!set.contains(c2)) {
+            set.add(c2);
+            map.put(c1, set);
+            indegreeMap.put(c2, indegreeMap.get(c2)+1);
+          }
+          break; // we break so that we can skip to the next consecutive 2 words
+        }
+      }
+    }
+
+    // Now we use Kahn's algorithm for topologica sorting
+    for (char c : indegreeMap.keySet()) {
+      if (indegreeMap.get(c) == 0) queue.add(c);
+    }
+
+    while (!queue.isEmpty()) {
+      char c = queue.poll();
+      result += c;
+      if (map.containsKey(c)) {
+        for (char c2 : map.get(c)) {
+          int updatedIndegree = indegreeMap.get(c2) - 1;
+          indegreeMap.put(c2, updatedIndegree);
+          if (updatedIndegree == 0) queue.add(c2);
+
+        }
+      }
+    }
+    // if there is a cycle, we return the empty string
+    if (result.length() != indegreeMap.size()) return "";
+
+    return result;
+  }
+
+  public static void main(String[] args) {
+    String[] words1 = {"wrt", "wrf", "er", "ett", "rftt"};
+    String[] words2 = {"z", "x"};
+    String[] words3 = {"z", "x", "z"};
+
+    System.out.println("alienOrder([\"wrt\", \"wrf\", \"er\", \"ett\", \"rftt\"]) = " + alienOrder(words1));
+    System.out.println("alienOrder([\"z\", \"x\"]) = " + alienOrder(words2));
+    System.out.println("alienOrder([\"z\", \"x\", \"z\"]) = " + alienOrder(words3));
+  }
+}
