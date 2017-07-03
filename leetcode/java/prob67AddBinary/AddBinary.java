af22d7ad1f8997c0771cb05c7b4bcc682a1ec3e3
AmadouSallah <amadousallah13@gmail.com>
2017-07-02 23:28:45 -0700

    Added problem statement for AddBinary.java

---
+++ leetcode/java/prob67AddBinary/AddBinary.java
@@ -0,0 +1,12 @@
+/*
+https://leetcode.com/problems/add-binary/#/description
+
+Problem 67: Add Binary
+
+Given two binary strings, return their sum (also a binary string).
+
+For example,
+a = "11"
+b = "1"
+Return "100".
+*/
