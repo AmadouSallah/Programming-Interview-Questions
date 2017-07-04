7ee15153d451a5dc257d79ac3ba92d5fbdd9d8e1
AmadouSallah <amadousallah13@gmail.com>
2017-07-03 22:43:23 -0700

    Added problem statement for SparseMatrixMultiplication.java

---
+++ leetcode/java/prob311SparseMatrixMultiplication/SparseMatrixMultiplication.java
@@ -0,0 +1,27 @@
+/*
+Resources: https://leetcode.com/problems/sparse-matrix-multiplication/#/description
+
+Problem 311. Sparse Matrix Multiplication
+
+Given two sparse matrices A and B, return the result of AB.
+
+You may assume that A's column number is equal to B's row number.
+
+Example:
+
+A = [
+  [ 1, 0, 0],
+  [-1, 0, 3]
+]
+
+B = [
+  [ 7, 0, 0 ],
+  [ 0, 0, 0 ],
+  [ 0, 0, 1 ]
+]
+
+
+     |  1 0 0 |   | 7 0 0 |   |  7 0 0 |
+AB = | -1 0 3 | x | 0 0 0 | = | -7 0 3 |
+                  | 0 0 1 |
+*/
