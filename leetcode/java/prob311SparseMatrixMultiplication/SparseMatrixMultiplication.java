61d90dbba7f5bdffc1944a1f46a0b4a945a34df7
AmadouSallah <amadousallah13@gmail.com>
2017-07-03 22:50:50 -0700

    Added solution for SparseMatrixMultiplication.java

--- leetcode/java/prob311SparseMatrixMultiplication/SparseMatrixMultiplication.java
+++ leetcode/java/prob311SparseMatrixMultiplication/SparseMatrixMultiplication.java
@@ -25,3 +25,23 @@ B = [
 AB = | -1 0 3 | x | 0 0 0 | = | -7 0 3 |
                   | 0 0 1 |
 */
+
+public class SparseMatrixMultiplication {
+  public int[][] multiply(int[][] A, int[][] B) {
+    int rowsA = A.length, colsB = B[0].length, n = A[0].length; // n is also B.length
+    int[][] result = new int[rowsA][colsB];
+
+    for (int row = 0; row < rowsA; row++) {
+      for (int i = 0; i < n; i++) {
+        if (A[row][i] != 0) {
+          for (int col = 0; col < colsB; col++) {
+            if (B[i][col] != 0) {
+              result[row][col] += A[row][i] * B[i][col];
+            }
+          }
+        }
+      }
+    }
+    return result;
+  }
+}
