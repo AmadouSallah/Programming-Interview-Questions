5546dd6f0513497d280c425669d5c7219d4b97e7
AmadouSallah <amadousallah13@gmail.com>
2017-07-03 17:03:38 -0700

    Added problem statement for removeInvalidParentheses.java

---
+++ leetcode/java/prob301RemoveInvalidParentheses/removeInvalidParentheses.java
@@ -0,0 +1,15 @@
+/*
+https://leetcode.com/problems/remove-invalid-parentheses/#/description
+
+Problem 301. Remove Invalid Parentheses
+
+Remove the minimum number of invalid parentheses in order to make the input string valid. Return all possible results.
+
+Note: The input string may contain letters other than the parentheses ( and ).
+
+Examples:
+"()())()" -> ["()()()", "(())()"]
+"(a)())()" -> ["(a)()()", "(a())()"]
+")(" -> [""]
+
+*/
