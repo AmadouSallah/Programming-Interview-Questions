92ec2f8513ba6ccb2e91e7ff4edc55f5febe2f67
AmadouSallah <amadousallah13@gmail.com>
2017-07-09 20:09:27 -0700

    Added Solution2 for NumberOfDecodings.java: O(n) runtime and O(1) space complexities

--- leetcode/java/prob91DecodeWays/NumberOfDecodings.java
+++ leetcode/java/prob91DecodeWays/NumberOfDecodings.java
@@ -16,6 +16,8 @@ The number of ways decoding "12" is 2.
 */
 
 public class NumberOfDecodings {
+
+  // O(n) runtime and O(n) space complexities
   public static int numDecodings(String s) {
     if (s == null || s.length() == 0) return 0;
     int n = s.length();
@@ -40,10 +42,43 @@ public class NumberOfDecodings {
     return dp[n];
   }
 
+  // O(n) runtime and O(1) space complexities
+  public static int numDecodings2(String s) {
+    if (s == null || s.length() == 0) return 0;
+    int n = s.length();
+    int p1 = 1, p2 = 0, temp;
+
+    if (s.charAt(0) != '0')  // There is only 1 way to decode a nonempty string of length 1:
+        p2 = 1;           // Example s = "3" -> "C", so only 1 way to decode "3"
+
+    for (int i = 2; i <= n; i++) {
+      temp = 0;
+      if (s.charAt(i-1) != '0') {
+          temp += p2;
+      }
+
+      int twoDigits = Integer.parseInt(s.substring(i-2, i));
+      if (twoDigits >= 10 && twoDigits <= 26) {
+          temp += p1;
+      }
+
+      p1 = p2;
+      p2 = temp;
+    }
+    return p2;
+  }
+
   public static void main(String[] args) {
+    System.out.println("Using solution1: O(n) runtime and O(n) space complexities");
     System.out.println("numDecodings(\"\") = " + numDecodings("")); // -> ""
     System.out.println("numDecodings(\"1\") = " + numDecodings("1")); // -> "A"
     System.out.println("numDecodings(\"12\") = " + numDecodings("12")); // -> "AB", or "L"
     System.out.println("numDecodings(\"123\") = " + numDecodings("123")); // -> "ABC", or "LX", or "LC"
+
+    System.out.println("\nUsing solution2: O(n) runtime and O(1) space complexities");
+    System.out.println("numDecodings2(\"\") = " + numDecodings2("")); // -> ""
+    System.out.println("numDecodings2(\"1\") = " + numDecodings2("1")); // -> "A"
+    System.out.println("numDecodings2(\"12\") = " + numDecodings2("12")); // -> "AB", or "L"
+    System.out.println("numDecodings2(\"123\") = " + numDecodings2("123")); // -> "ABC", or "LX", or "LC"
   }
 }
