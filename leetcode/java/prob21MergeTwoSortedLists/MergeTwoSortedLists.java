2d0fcbc4825568619ed982d546ec3522fa7d38fb
AmadouSallah <amadousallah13@gmail.com>
2017-07-12 14:24:25 -0700

    Added Iterative solution for MergeTwoSortedLists.java

--- leetcode/java/prob21MergeTwoSortedLists/MergeTwoSortedLists.java
+++ leetcode/java/prob21MergeTwoSortedLists/MergeTwoSortedLists.java
@@ -30,4 +30,29 @@ public class MergeTwoSortedLists {
       return l2;
     }
   }
+
+  // Iterative solution
+  public ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+    if (l1 == null && l2 == null) return null;
+    else if (l1 == null) return l2;
+    else if (l2 == null) return l1;
+
+    ListNode curr = new ListNode(0);
+    ListNode node = curr;
+    while (l1 != null && l2 != null) {
+      if (l1.val < l2.val) {
+        curr.next = l1;
+        l1 = l1.next;
+      } else {
+          curr.next = l2;
+          l2 = l2.next;
+      }
+      curr = curr.next;
+    }
+
+    if (l1 != null) curr.next = l1;
+    if (l2 != null) curr.next = l2;
+
+    return node.next;
+  }
 }
