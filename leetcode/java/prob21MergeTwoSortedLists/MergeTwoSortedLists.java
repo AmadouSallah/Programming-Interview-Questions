836ac0c655e9cfe35e308d36c9bd3db3f2fd31e1
AmadouSallah <amadousallah13@gmail.com>
2017-07-12 14:14:44 -0700

    Added problem statement for MergeTwoSortedLists.java

---
+++ leetcode/java/prob21MergeTwoSortedLists/MergeTwoSortedLists.java
@@ -0,0 +1,10 @@
+/*
+Resource: https://leetcode.com/problems/merge-two-sorted-lists/#/description
+
+Problem 21. Merge Two Sorted Lists
+
+Merge two sorted linked lists and return it as a new list. The new list should be made by splicing
+together the nodes of the first two lists.
+
+
+*/
