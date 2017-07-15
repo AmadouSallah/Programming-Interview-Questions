85440c45dce1c98368eeb6a4899d9489997e221d
AmadouSallah <amadousallah13@gmail.com>
2017-07-15 15:41:36 -0700

    Added problem statement for MergeKSortedLists.java

---
+++ leetcode/java/prob23MergeKSortedLists/MergeKSortedLists.java
@@ -0,0 +1,7 @@
+/*
+Resource: https://leetcode.com/problems/merge-k-sorted-lists/#/description
+
+Problem 23. Merge k Sorted Lists
+
+Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+*/
