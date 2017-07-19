8310c7c5169935bace9d558f2946202bd3139d36
AmadouSallah <amadousallah13@gmail.com>
2017-07-19 15:04:53 -0700

    Added solution for RotateImageBy90Degrees.java

--- leetcode/java/prob48RotateImageBy90Degrees/RotateImageBy90Degrees.java
+++ leetcode/java/prob48RotateImageBy90Degrees/RotateImageBy90Degrees.java
@@ -10,3 +10,19 @@ Rotate the image by 90 degrees (clockwise).
 Follow up:
 Could you do this in-place?
 */
+
+public class RotateImageBy90Degrees {
+  public void rotate(int[][] matrix) {
+   if (matrix == null) return;
+    int n = matrix.length;
+    for (int i = 0; i < n/2; i++) {
+      for (int j = i; j < n-1-i; j++) {
+        int temp = matrix[i][j]; // saving top
+        matrix[i][j] = matrix[n-j-1][i]; // left -> top
+        matrix[n-j-1][i] = matrix[n-i-1][n-j-1]; // bottom -> left
+        matrix[n-i-1][n-j-1] = matrix[j][n-i-1]; // right -> bottom
+        matrix[j][n-i-1] = temp; // top -> right
+      }
+    }
+  }
+}
