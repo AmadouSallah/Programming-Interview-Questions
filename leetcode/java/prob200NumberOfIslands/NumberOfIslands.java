3e8041f13d50f862764d3cd0487f032b6ecf8e8f
AmadouSallah <amadousallah13@gmail.com>
2017-07-19 01:15:19 -0700

    Added solution for NumberOfIslands.java

--- leetcode/java/prob200NumberOfIslands/NumberOfIslands.java
+++ leetcode/java/prob200NumberOfIslands/NumberOfIslands.java
@@ -23,3 +23,38 @@ Example 2:
 00011
 Answer: 3
 */
+
+public class NumberOfIslands {
+  public static int numberOfIslands(char[][] grid) {
+    if (grid == null || grid.length == 0) return 0;
+    int rows = grid.length, cols = grid[0].length, count = 0;
+    for(int i = 0; i < rows; i++) {
+      for (int j = 0; j < cols; j++) {
+        if (grid[i][j] == '1') {
+          count++;
+          dfs(grid, i, j);
+        }
+      }
+    }
+    return count;
+  }
+
+  public static void dfs(char[][] grid, int i, int j) {
+    int rows = grid.length, cols = grid[0].length;
+    if (i < 0 || j < 0 || i >= rows || j >= cols || grid[i][j] == '0') {
+      return;
+    }
+    grid[i][j] = '0';
+    dfs(grid, i-1, j);
+    dfs(grid, i+1, j);
+    dfs(grid, i, j-1);
+    dfs(grid, i, j+1);
+  }
+
+  public static void main(String[] args) {
+    char[][] grid1 = {{'1', '1', '1', '1', '0'}, {'1', '1', '0', '1', '0'}, {'1', '1', '0', '0', '0'}, {'0', '0', '0', '0', '0'}};
+    char[][] grid2 = {{'1', '1', '0', '0', '0'}, {'1', '1', '0', '0', '0'}, {'0', '0', '1', '0', '0'}, {'0', '0', '0', '1', '1'}};
+    System.out.println(numberOfIslands(grid1));
+    System.out.println(numberOfIslands(grid2));
+  }
+}
