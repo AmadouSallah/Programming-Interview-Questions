5bb54b16c3e95894ee66bbc0a05b1b983e5af999
AmadouSallah <amadousallah13@gmail.com>
2017-08-05 10:57:01 -0700

    Added problem statement for Trie.java

---
+++ leetcode/java/prob208ImplementTrie/Trie.java
@@ -0,0 +1,10 @@
+/*
+Resources: https://leetcode.com/problems/implement-trie-prefix-tree/description/
+
+Problem 208: Implement Trie (Prefix Tree)
+
+Implement a trie with insert, search, and startsWith methods.
+
+Note:
+You may assume that all inputs are consist of lowercase letters a-z.
+*/
