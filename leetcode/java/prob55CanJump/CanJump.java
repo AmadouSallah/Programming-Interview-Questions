46c17bb043cba75010b44ef1d5920b9f5457ea10
AmadouSallah <amadousallah13@gmail.com>
2017-10-15 22:46:29 -0700

    Added solution for CanJump.java

--- leetcode/java/prob55CanJump/CanJump.java
+++ leetcode/java/prob55CanJump/CanJump.java
@@ -12,3 +12,20 @@ A = [2,3,1,1,4], return true.
 
 A = [3,2,1,0,4], return false.
 */
+
+public class CanJump {
+  public static boolean canJump(int[] nums) {
+    int n = nums.length, position = n-1;
+    for (int i = n-1; i >= 0; i--) {
+      if (i + nums[i] >= position) {
+        position = i;
+      }
+    }
+    return position == 0;
+  }
+
+  public static void main(String[] args) {
+    System.out.println("canJump[2, 3, 1, 1, 4] = " + canJump(new int[] {2, 3, 1, 1, 4}));
+    System.out.println("canJump[3, 2, 1, 0, 4] = " + canJump(new int[] {3, 2, 1, 0, 4}));
+  }
+}
