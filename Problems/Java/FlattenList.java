a6676e7c81b8220957a8963d00e12ddafbb5fc82
AmadouSallah <amadousallah13@gmail.com>
2017-10-18 13:46:59 -0700

    Added problem statement for FlattenList.java

---
+++ Problems/Java/FlattenList.java
@@ -0,0 +1,14 @@
+/*
+Given a list that contains a nested lists, write a funtion to flatten it.
+
+Examples:
+
+input = [1, [2, 3], 4]
+output = [1, 2, 3, 4]
+
+input = [1, [2, 3], 4, [5, 6, 7]]
+output = [1, 2, 3, 4, 5, 6, 7]
+
+input = [[1], [2, [3, [4]]], 5]
+result = [1, 2, 3, 4, 5]
+*/
