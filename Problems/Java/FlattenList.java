000d06713775efcf2dd78627d35380652e3e60d2
AmadouSallah <amadousallah13@gmail.com>
2017-10-18 14:30:07 -0700

    Added iterative solution for FlattenList.java

--- Problems/Java/FlattenList.java
+++ Problems/Java/FlattenList.java
@@ -16,6 +16,7 @@ result = [1, 2, 3, 4, 5]
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.ListIterator;
 import java.util.Arrays;
 
 public class FlattenList {
@@ -41,6 +42,33 @@ public class FlattenList {
     }
   }
 
+  public static List<Integer> flattenIterative(List<?> list) {
+    List<Object> tempList = null;
+    List newList = new ArrayList<>();
+    newList.addAll(list);
+
+    ListIterator itr = newList.listIterator();
+    while (itr.hasNext()) {
+      Object obj = itr.next();
+
+      if (obj instanceof List) { // if obj is a list, then
+        tempList = (List) obj; // we save it to tempList
+        itr.remove(); // we remove it from newList
+
+        // Now add each item of tempList, i.e of obj, to newList,
+        // (this adds at the same position where obj was removed)
+        for (Object o : tempList) {
+          itr.add(o);
+        }
+
+        // Reset the iterator, itr
+        itr = newList.listIterator();
+      }
+    }
+
+    return newList;
+  }
+
   public static void main(String[] args) {
     // list1 = [1, [2, 3], 4]
     // list2 = [1, [2, 3], 4, [5, 6, 7]];
@@ -48,18 +76,20 @@ public class FlattenList {
     List<?> list1 = Arrays.asList(1, Arrays.asList(2, 3), 4);
     List<Object> list2 = Arrays.asList(1, Arrays.asList(2, 3), 4, Arrays.asList(5, 6, 7));
     List<Object> list3 = Arrays.asList( Arrays.asList(1), Arrays.asList(2, Arrays.asList(3, Arrays.asList(4))), 5 );
-    // System.out.println(list);
-    // System.out.println(list2);
 
     System.out.println("flattenRecursive([1, [2, 3], 4]) = " + flattenRecursive(list1));
-    // System.out.println("flattenIterative([1, [2, 3], 4]) = " + flattenIterative(list1));
+    System.out.println("flattenIterative([1, [2, 3], 4]) = " + flattenIterative(list1));
 
-    System.out.println("flattenRecursive([1, [2, 3], 4, [5, 6, 7]]) = " + flattenRecursive(list2));
-    // System.out.println("flattenIterative([1, [2, 3], 4, [5, 6, 7]]) = " + flattenIterative(list2));
+    System.out.println("\nflattenRecursive([1, [2, 3], 4, [5, 6, 7]]) = " + flattenRecursive(list2));
+    System.out.println("flattenIterative([1, [2, 3], 4, [5, 6, 7]]) = " + flattenIterative(list2));
 
-    System.out.println("flattenRecursive([[1], [2, [3, [4]]], 5]) = " + flattenRecursive(list3));
-    // System.out.println("flattenIterative([[1], [2, [3, [4]]], 5]) = " + flattenIterative(list3));
-
-
+    System.out.println("\nflattenRecursive([[1], [2, [3, [4]]], 5]) = " + flattenRecursive(list3));
+    System.out.println("flattenIterative([[1], [2, [3, [4]]], 5]) = " + flattenIterative(list3));
   }
 }
+
+/*
+Resources:
+https://evanhoffman.com/2010/04/17/code-to-flatten-a-list-in-java-with-and-without-recursion/
+https://gist.github.com/seanzhu/11167427
+*/
