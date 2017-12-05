a0aa7c4a0faff41a8f8cc2f29d23387ca37047dd
AmadouSallah <amadousallah13@gmail.com>
2017-12-04 17:31:15 -0800

    Added solution for PlusOne.java

--- leetcode/java/prob66PlusOne/PlusOne.java
+++ leetcode/java/prob66PlusOne/PlusOne.java
@@ -10,3 +10,21 @@ You may assume the integer do not contain any leading zero, except the number 0 
 
 The digits are stored such that the most significant digit is at the head of the list.
 */
+
+public class PlusOne {
+
+  public int[] plusOne(int[] digits) {
+    int n = digits.length;
+    for (int i = n-1; i >= 0; i--) {
+      if (digits[i] < 9) {
+        digits[i]++;
+        return digits;
+      }
+      digits[i] = 0;
+    }
+    int[] result = new int[n+1];
+    result[0] = 1;
+    return result;
+  }
+
+}
