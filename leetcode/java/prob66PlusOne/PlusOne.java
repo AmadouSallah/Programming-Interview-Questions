535a726d3719d5f8f791d783a8cd0ec028895aa0
AmadouSallah <amadousallah13@gmail.com>
2017-12-04 17:25:10 -0800

    Added problem statement for PlusOne.java

---
+++ leetcode/java/prob66PlusOne/PlusOne.java
@@ -0,0 +1,12 @@
+/*
+
+From https://leetcode.com/problems/plus-one/description/
+
+Leetcode Problem 66: Plus one
+
+Given a non-negative integer represented as a non-empty array of digits, plus one to the integer.
+
+You may assume the integer do not contain any leading zero, except the number 0 itself.
+
+The digits are stored such that the most significant digit is at the head of the list.
+*/
