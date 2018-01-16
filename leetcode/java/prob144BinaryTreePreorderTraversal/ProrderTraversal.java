7dd1c819f8454ebc12b3f45beed83ff319263c3b
AmadouSallah <amadousallah13@gmail.com>
2018-01-16 15:55:50 -0800

    Added solution for ProrderTraversal.java

--- leetcode/java/prob144BinaryTreePreorderTraversal/ProrderTraversal.java
+++ leetcode/java/prob144BinaryTreePreorderTraversal/ProrderTraversal.java
@@ -15,3 +15,30 @@ return [1, 2, 3]
 
 Note: Recursive solution is trivial, could you do it iteratively?
 */
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class ProrderTraversal {
+  private class TreeNode {
+    int val;
+    TreeNode left, right;
+    TreeNode(int x) { val = x;}
+  }
+
+  public List<Integer> preorderTraversal(TreeNode root) {
+    List<Integer> result = new ArrayList<>();
+    if (root == null) return result;
+
+    Stack<TreeNode> stack = new Stack<>();
+    stack.push(root);
+    while (!stack.isEmpty()) {
+      TreeNode current = stack.pop();
+      result.add(current.val);
+      if (current.right != null) stack.push(current.right);
+      if (current.left != null) stack.push(current.left);
+    }
+    return result;
+  }
+}
