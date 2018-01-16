cfbf0cd38b2a328ff6d705be37441e996503ae63
AmadouSallah <amadousallah13@gmail.com>
2018-01-16 15:52:05 -0800

    Added problem statement for ProrderTraversal.java

---
+++ leetcode/java/prob144BinaryTreePreorderTraversal/ProrderTraversal.java
@@ -0,0 +1,17 @@
+/*
+Leetcode Problem 144. Binary Tree Preorder Traversal
+
+https://leetcode.com/problems/binary-tree-preorder-traversal/description/
+
+Given a binary tree, return the preorder traversal of its nodes' values.
+
+For example:
+1
+  \
+   2
+  /
+3
+return [1, 2, 3]
+
+Note: Recursive solution is trivial, could you do it iteratively?
+*/
