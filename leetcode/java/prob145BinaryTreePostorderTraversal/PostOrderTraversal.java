ee14f6597c5a81555d02c541e7468386059dceed
AmadouSallah <amadousallah13@gmail.com>
2018-03-10 14:36:34 -0800

    Added solution for PostOrderTraversal.java

--- leetcode/java/prob145BinaryTreePostorderTraversal/PostOrderTraversal.java
+++ leetcode/java/prob145BinaryTreePostorderTraversal/PostOrderTraversal.java
@@ -17,3 +17,39 @@ return [3,2,1].
 
 Note: Recursive solution is trivial, could you do it iteratively?
 */
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class PostOrderTraversal {
+
+  public List<Integer> postorderTraversal(TreeNode root) {
+    List<Integer> result = new ArrayList<>();
+
+    if (root == null) return result;
+
+    Stack<TreeNode> stack = new Stack();
+    stack.push(root);
+    TreeNode current;
+
+    while (!stack.isEmpty()) {
+      current = stack.pop();
+      result.add(0, current.val);
+
+      if (current.left != null) stack.push(current.left);
+      if (current.right != null) stack.push(current.right);
+    }
+
+    return result;
+  }
+
+  private static class TreeNode {
+    int val;
+    TreeNode left, right;
+
+    public TreeNode(int val) {
+      this.val = val;
+    }
+  }
+}
