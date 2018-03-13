2a6914b65aeb60572ca185a1f08481df1e866d52
AmadouSallah <amadousallah13@gmail.com>
2018-03-12 22:17:30 -0700

    Added 2nd solution for IsAnagram.java using HashMap

--- leetcode/java/prob242ValidAnagram/IsAnagram.java
+++ leetcode/java/prob242ValidAnagram/IsAnagram.java
@@ -14,6 +14,8 @@ What if the inputs contain unicode characters? How would you adapt your solution
 */
 
 import java.util.Arrays;
+import java.util.Map;
+import java.util.HashMap;
 
 public class IsAnagram {
   public static boolean isAnagram(String s, String t) {
@@ -36,6 +38,35 @@ public class IsAnagram {
     return true;
   }
 
+  public static boolean isAnagram2(String s, String t) {
+
+      Map<Character, Integer> map = new HashMap<>();
+
+      for (int i = 0; i < s.length(); i++) {
+        char c = s.charAt(i);
+        if (map.containsKey(c)) {
+          map.put(c, map.get(c) + 1);
+        } else {
+          map.put(c, 1);
+        }
+      }
+
+      for (int i = 0; i < t.length(); i++) {
+        char c = t.charAt(i);
+        if (!map.containsKey(c)) {
+          return false;
+        }
+
+        map.put(c, map.get(c) - 1);
+
+        if (map.get(c) == 0) {
+          map.remove(c);
+        }
+      }
+
+      return map.isEmpty();
+  }
+
   public static void main(String[] args) {
     System.out.println("isAnagram(\"\", \"\") = " + isAnagram("", ""));
     System.out.println("isAnagram(\"a\", \"\") = " + isAnagram("a", ""));
@@ -44,5 +75,14 @@ public class IsAnagram {
     System.out.println("isAnagram(\"anagram\", \"nagaram\") = " + isAnagram("anagram", "nagaram"));
     System.out.println("isAnagram(\"rat\", \"car\") = " + isAnagram("rat", "car"));
     System.out.println("isAnagram(\"hello\", \"hi\") = " + isAnagram("hello", "hi"));
+
+    System.out.println("\nTesting with solutio using HashMap:");
+    System.out.println("isAnagram2(\"\", \"\") = " + isAnagram2("", ""));
+    System.out.println("isAnagram2(\"a\", \"\") = " + isAnagram2("a", ""));
+    System.out.println("isAnagram2(\"\", \"b\") = " + isAnagram2("", "b"));
+    System.out.println("isAnagram2(\"a\", \"b\") = " + isAnagram2("a", "b"));
+    System.out.println("isAnagram2(\"anagram\", \"nagaram\") = " + isAnagram2("anagram", "nagaram"));
+    System.out.println("isAnagram2(\"rat\", \"car\") = " + isAnagram2("rat", "car"));
+    System.out.println("isAnagram2(\"hello\", \"hi\") = " + isAnagram2("hello", "hi"));
     }
 }
