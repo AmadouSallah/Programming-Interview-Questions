dac93cc6e948f83bbd87eb232468184da339e125
AmadouSallah <amadousallah13@gmail.com>
2018-03-13 11:41:17 -0700

    Added solution for GroupAnagrams.java

--- leetcode/java/prob49GroupAnagrams/GroupAnagrams.java
+++ leetcode/java/prob49GroupAnagrams/GroupAnagrams.java
@@ -14,3 +14,40 @@ Return:
 ]
 Note: All inputs will be in lower-case.
 */
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Arrays;
+
+public class GroupAnagrams {
+  // O(n*klog(k)) runtime where k is the lenght of the longest string in input strs
+  public static List<List<String>> groupAnagrams(String[] strs) {
+    List<List<String>> result = new ArrayList<>();
+    if (strs == null || strs.length == 0) return result;
+
+    Map<String, List> map = new HashMap<>();
+
+    for (String s : strs) {
+      char[] arr = s.toCharArray();
+      Arrays.sort(arr);
+      String key = String.valueOf(arr);
+
+      if (!map.containsKey(key)) {
+        map.put(key, new ArrayList<>());
+      }
+      map.get(key).add(s);
+    }
+    for (String key : map.keySet()) {
+      result.add(map.get(key));
+    }
+    return result;
+
+  }
+
+  public static void main(String[] args) {
+    String[] input = new String[] {"eat", "tea", "tan", "ate", "nat", "bat"};
+    System.out.println("groupAnagrams([\"eat\", \"tea\", \"tan\", \"ate\", \"nat\", \"bat\"]) = " + groupAnagrams(input));
+  }
+}
