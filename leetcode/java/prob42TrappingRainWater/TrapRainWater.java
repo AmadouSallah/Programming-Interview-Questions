c9e24f93708765db06befbd93a5657bb0c1a1c92
AmadouSallah <amadousallah13@gmail.com>
2018-04-04 23:41:15 -0700

    Added Dynamic Solution for TrapRainWater.java: O(n) runtime and O(n) space complexities

--- leetcode/java/prob42TrappingRainWater/TrapRainWater.java
+++ leetcode/java/prob42TrappingRainWater/TrapRainWater.java
@@ -17,13 +17,19 @@ We find the total amount of water that each array element can store, add them al
 To find the total amount of water that each array element can trap, we do the followings:
 a) For each element, we find the highest bar on both its left side (maxLeft) and right side (maxRight)
 b) We take the smaller of maxLeft and maxRight and subtract the current element
+
+Solution 2, Dynamic Programming: O(n) runtime and O(n) space complexities
+Instate of iterating over and over both the left and right sides of each array element to
+find the highest bar size up to that element, we store them in leftMax and rightMax arrays.
+For each array element at index i, leftMax[i] (respectively, rightMax[i]) is the largest bar to its
+left (respectively, right)
 */
 
 public class TrapRainWater {
 
   // Solution 1, Brute force: O(n2) runtime and O(1) space complexities
   public static int trap(int[] height) {
-    
+
     if (height == null || height.length == 0) return 0;
 
     int n = height.length, ans = 0, maxLeft, maxRight;
@@ -42,8 +48,36 @@ public class TrapRainWater {
     return ans;
   }
 
+  // Solution 2, Dynamic Programming: O(n) runtime and O(n) space complexities
+  public static int trap2(int[] height) {
+    if (height == null || height.length == 0) return 0;
+
+    int n = height.length, ans = 0;
+    // leftMax[i] (respectively, rightMax[i]) is the largest bar to the left (respectively, right) of height[i]
+    int[] leftMax = new int[n], rightMax = new int[n];
+    leftMax[0] = height[0];
+    rightMax[n-1] = height[n-1];
+
+    // fill leftMax array
+    for (int i = 1; i < n; i++) {
+      leftMax[i] = Math.max(height[i], leftMax[i-1]);
+    }
+    // fill rightMax array
+    for (int i = n-2; i >= 0; i--) {
+      rightMax[i] = Math.max(height[i], rightMax[i+1]);
+    }
+
+    // Find the accumulated water on each bar and add it to answer
+    for (int i = 0; i < n; i++) {
+      int smallestBar = Math.min(leftMax[i], rightMax[i]);
+      ans += smallestBar - height[i];
+    }
+    return ans;
+  }
+
   public static void main(String[] args) {
     int[] heights = new int[] {0,1,0,2,1,0,1,3,2,1,2,1};
     System.out.println("trap([0,1,0,2,1,0,1,3,2,1,2,1]) = " + trap(heights));
+    System.out.println("trap2([0,1,0,2,1,0,1,3,2,1,2,1]) = " + trap2(heights));
   }
 }
