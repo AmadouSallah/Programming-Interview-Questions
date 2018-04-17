1b6e237980add29f3e718aea8d135daaebbf9352
AmadouSallah <amadousallah13@gmail.com>
2018-04-17 15:02:19 -0700

    Added insert method to Trie.java

--- Data_Structures_And_Algorithms/Trie/Trie.java
+++ Data_Structures_And_Algorithms/Trie/Trie.java
@@ -11,6 +11,21 @@ public class Trie {
     root = new TrieNode();
   }
 
+  // Inserts a word into the trie.
+  // Complexity: O(n) runtime and O(1) space, where n is length of the input word,
+  public static void insert(String word) {
+    TrieNode current = root;
+    int n = word.length();
+    for (int i = 0; i < n; i++) {
+      char c = word.charAt(i);
+      if (!current.children.containsKey(c)) {
+        current.children.put(c, new TrieNode());
+      }
+      current = current.children.get(c);
+    }
+    current.isEnd = true;
+  }
+
   public static class TrieNode {
     Map<Character, TrieNode> children;
     boolean isEnd;
