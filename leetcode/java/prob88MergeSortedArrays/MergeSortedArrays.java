1ddea6e6277f1b770b3a547bb8514c14663d2a89
AmadouSallah <amadousallah13@gmail.com>
2018-05-01 12:28:42 -0700

    Added solution for MergeSortedArrays.java

--- leetcode/java/prob88MergeSortedArrays/MergeSortedArrays.java
+++ leetcode/java/prob88MergeSortedArrays/MergeSortedArrays.java
@@ -19,3 +19,15 @@ nums2 = [2,5,6],       n = 3
 
 Output: [1,2,2,3,5,6]
 */
+
+public class MergeSortedArrays {
+  public void merge(int[] nums1, int m, int[] nums2, int n) {
+    int i = m-1, j = n-1, k = m+n-1;
+    while (k >= 0) {
+        if (j < 0 || (i >=0 && nums1[i] > nums2[j]))
+          nums1[k--] = nums1[i--];
+        else
+          nums1[k--] = nums2[j--];
+    }
+  }
+}
