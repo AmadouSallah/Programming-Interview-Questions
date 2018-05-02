e7734912a147b644da5c40b28f2701cd1ebd6490
AmadouSallah <amadousallah13@gmail.com>
2018-05-02 03:02:02 -0700

    Added solution for ReverseString.java

--- leetcode/java/prob344ReverseString/ReverseString.java
+++ leetcode/java/prob344ReverseString/ReverseString.java
@@ -8,3 +8,25 @@ Write a function that takes a string as input and returns the string reversed.
 Example:
 Given s = "hello", return "olleh".
 */
+
+public class ReverseString {
+  public static String reverseString(String s) {
+    char[] charArray = s.toCharArray();
+    int left = 0, right = s.length()-1;
+
+    while (left < right) {
+      char temp = charArray[left];
+      charArray[left] = charArray[right];
+      charArray[right] = temp;
+
+      left++;
+      right--;
+    }
+
+    return new String(charArray);
+  }
+
+  public static void main(String[] args) {
+    System.out.println("reverseString(\"hello\") = " + reverseString("hello"));
+  }
+}
