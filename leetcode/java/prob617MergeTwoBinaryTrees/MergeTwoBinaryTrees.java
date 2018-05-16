d865c519d58cf020ebf7f7e226476f70a7a1d48d
AmadouSallah <amadousallah13@gmail.com>
2018-05-16 15:15:04 -0700

    Added iterative solution for MergeTwoBinaryTrees.java: O(n) runtime and O(n) space complexities, where n is the minimum number of nodes from the 2 input trees

--- leetcode/java/prob617MergeTwoBinaryTrees/MergeTwoBinaryTrees.java
+++ leetcode/java/prob617MergeTwoBinaryTrees/MergeTwoBinaryTrees.java
@@ -29,6 +29,8 @@ Merged tree:
 Note: The merging process must start from the root nodes of both trees.
 */
 
+import java.util.Stack;
+
 public class MergeTwoBinaryTrees {
 
   public static class TreeNode {
@@ -40,13 +42,45 @@ public class MergeTwoBinaryTrees {
     }
   }
 
-  // O(n) Runtime and O(n) space complexities where n is the minimum number of nodes from the 2 input trees
+  // Recursive Solution: O(n) Runtime where n is the minimum number of nodes from the 2 input trees
+  // Space complexities - depth of the recursion: worst case is O(n) and average case is O(log n).
   public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
     if (t1 == null) return t2;
     if (t2 == null) return t1;
     t1.val += t2.val;
     t1.left = mergeTrees(t1.left, t2.left);
     t1.right = mergeTrees(t1.right, t2.right);
+    return t1;
+  }
+
+  // Iterative Solutioh: O(n) Runtime and O(n) space complexities
+  // where n is the minimum number of nodes from the 2 input trees
+  public TreeNode mergeTrees2(TreeNode t1, TreeNode t2) {
+
+    if (t1 == null) return t2;
+
+    Stack<TreeNode[]> stack = new Stack<>();
+    stack.push(new TreeNode[] {t1, t2});
+
+    while (!stack.isEmpty()) {
+      TreeNode[] t = stack.pop();
+
+      if (t[0] == null || t[1] == null) continue;
+
+      t[0].val += t[1].val;
+
+      if (t[0].left == null) {
+        t[0].left = t[1].left;
+      } else {
+        stack.push(new TreeNode[] {t[0].left, t[1].left});
+      }
+
+      if (t[0].right == null) {
+        t[0].right = t[1].right;
+      } else {
+        stack.push(new TreeNode[] {t[0].right, t[1].right});
+      }
+    }
     return t1;
   }
 }
