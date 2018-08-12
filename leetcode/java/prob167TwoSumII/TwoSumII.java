c612cd294eebbcc6ef5c187a3f280c38cae5a611
AmadouSallah <amadousallah13@gmail.com>
2018-08-12 14:51:15 -0700

    Added solution for TwoSumII.java

--- leetcode/java/prob167TwoSumII/TwoSumII.java
+++ leetcode/java/prob167TwoSumII/TwoSumII.java
@@ -19,3 +19,36 @@ Output: [1,2]
 
 Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
 */
+
+public class TwoSumII {
+  public static int[] twoSumII(int[] numbers, int target) {
+    if (numbers == null || numbers.length < 2) return new int[0];
+    int n = numbers.length, left = 0, right = n-1, sum = 0;
+    while (left < right) {
+      sum = numbers[left] + numbers[right];
+      if (sum == target) {
+        return new int[] {left+1, right+1};
+      } else if (sum < target) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+    return new int[0];
+  }
+
+  public static String printArray(int[] arr) {
+    String s = "[";
+    int n = arr.length;
+    for (int i = 0; i < n-1; i++)
+
+      s += arr[i] + ", ";
+    s += (n > 0) ? (arr[n-1] + "]") : "]";
+    return s;
+  }
+
+  public static void main(String[] args) {
+    int[] arr = new int[] {2,7,11,15};
+    System.out.println("twoSumII([2,7,11,15], 9) = " + printArray(twoSumII(arr, 9)));
+  }
+}
