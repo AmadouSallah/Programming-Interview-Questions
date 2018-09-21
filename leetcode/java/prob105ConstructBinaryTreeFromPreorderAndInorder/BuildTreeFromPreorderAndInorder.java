96b10c8c03b2710b18b3f704ed096b1a0ffcf94a
AmadouSallah <amadousallah13@gmail.com>
2018-09-20 22:47:33 -0700

    Added 3rd solution for BuildTreeFromPreorderAndInorder.java

--- leetcode/java/prob105ConstructBinaryTreeFromPreorderAndInorder/BuildTreeFromPreorderAndInorder.java
+++ leetcode/java/prob105ConstructBinaryTreeFromPreorderAndInorder/BuildTreeFromPreorderAndInorder.java
@@ -53,6 +53,7 @@ public class BuildTreeFromPreorderAndInorder {
     return root;
   }
 
+  // Solution 2
   public static TreeNode buildTree2(int[] preorder, int[] inorder) {
     Map<Integer, Integer> map = new HashMap<>();
 
@@ -75,6 +76,30 @@ public class BuildTreeFromPreorderAndInorder {
     return root;
   }
 
+  // Solution 3
+  public static TreeNode buildTree3(int[] preorder, int[] inorder) {
+    Map<Integer, Integer> map = new HashMap<>();
+
+    for (int i = 0; i < inorder.length; i++) {
+      map.put(inorder[i], i);
+    }
+    return traverse3(preorder, inorder, map, 0, 0, preorder.length-1, inorder.length - 1);
+  }
+
+  public static TreeNode traverse3(int[] preorder, int[] inorder, Map<Integer, Integer> map, int preStart, int inStart, int preEnd, int inEnd) {
+    if (preStart > preEnd || inStart > inEnd) return null;
+
+    int rootVal = preorder[preStart];
+    int i = map.get(rootVal);
+    int leftLen = i - inStart; // the length of the left subtree
+    TreeNode root = new TreeNode(rootVal);
+
+    root.left = traverse3(preorder, inorder, map, preStart+1, inStart, preStart + leftLen, i-1);
+    root.right = traverse3(preorder, inorder, map, preStart+1+leftLen, i+1, preEnd, inEnd);
+
+    return root;
+  }
+
   public static void printInorder(TreeNode root) {
     if (root == null) return;
     printInorder(root.left);
@@ -86,16 +111,21 @@ public class BuildTreeFromPreorderAndInorder {
     int[] preorder = new int[] {3,9,20,15,7};
     int[] inorder = new int[] {9,3,15,20,7};
 
+    System.out.println("Solution 1:");
     TreeNode root = buildTree(preorder, inorder);
     System.out.println("Given preorder = [3,9,20,15,7] and inorder = [9,3,15,20,7], the inorder of the built tree is:");
-
     printInorder(root);
-    System.out.println("\n\nSolution 2: Using a hashmap:");
 
+    System.out.println("\n\nSolution 2:");
     TreeNode root2 = buildTree2(preorder, inorder);
     System.out.println("Given preorder = [3,9,20,15,7] and inorder = [9,3,15,20,7], the inorder of the built tree is:");
-
     printInorder(root2);
+
+    System.out.println("\n\nSolution 3:");
+    TreeNode root3 = buildTree2(preorder, inorder);
+    System.out.println("Given preorder = [3,9,20,15,7] and inorder = [9,3,15,20,7], the inorder of the built tree is:");
+    printInorder(root3);
+    
     System.out.println();
   }
 }
