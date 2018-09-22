43280ac228205321896b2c4d6572da289410553e
AmadouSallah <amadousallah13@gmail.com>
2018-09-21 23:39:47 -0700

    Added problem statement for BuildTreeFromInorderAndPostorder.java

---
+++ leetcode/java/prob106ConstructBinaryTreeFromInorderAndPostorder/BuildTreeFromInorderAndPostorder.java
@@ -0,0 +1,19 @@
+/*
+Leetcode Problem 106. Construct Binary Tree from Inorder and Postorder Traversal
+https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/description/
+
+Given inorder and postorder traversal of a tree, construct the binary tree.
+
+Note:
+You may assume that duplicates do not exist in the tree.
+
+For example, given
+inorder = [9,3,15,20,7]
+postorder = [9,15,7,20,3]
+Return the following binary tree:
+    3
+   / \
+  9  20
+    /  \
+   15   7
+*/
