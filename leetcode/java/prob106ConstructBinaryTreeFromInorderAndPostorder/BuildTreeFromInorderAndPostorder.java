5309bc2527e8b9e7aa60308eb0ce731811bfd39b
AmadouSallah <amadousallah13@gmail.com>
2018-09-21 23:43:45 -0700

    Added solution for BuildTreeFromInorderAndPostorder.java

--- leetcode/java/prob106ConstructBinaryTreeFromInorderAndPostorder/BuildTreeFromInorderAndPostorder.java
+++ leetcode/java/prob106ConstructBinaryTreeFromInorderAndPostorder/BuildTreeFromInorderAndPostorder.java
@@ -17,3 +17,61 @@ Return the following binary tree:
     /  \
    15   7
 */
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class BuildTreeFromInorderAndPostorder {
+  public static class TreeNode {
+    int val;
+    TreeNode left, right;
+    public TreeNode(int val) {
+      this.val = val;
+    }
+  }
+
+  public static TreeNode buildTree(int[] inorder, int[] postorder) {
+    Map<Integer, Integer> map = new HashMap<>();
+
+    for (int i = 0; i < inorder.length; i++) {
+      map.put(inorder[i], i);
+    }
+    return traverse(inorder, postorder, map, 0, 0, inorder.length - 1, postorder.length-1);
+  }
+
+  public static TreeNode traverse(int[] inorder, int[] postorder, Map<Integer, Integer> map, int inStart, int postStart, int inEnd, int postEnd) {
+    if (inStart > inEnd || postStart > postEnd) return null;
+
+    int rootVal = postorder[postEnd];
+    int i = map.get(rootVal);
+    int leftLen = i - inStart; // the length of the left subtree
+    TreeNode root = new TreeNode(rootVal);
+
+    root.left = traverse(inorder, postorder, map, inStart, postStart, i-1, postStart+leftLen-1);
+    root.right = traverse(inorder, postorder, map, i+1, postStart+leftLen, inEnd, postEnd-1);
+
+    return root;
+  }
+
+
+  public static void printInorder(TreeNode root) {
+    if (root == null) return;
+    printInorder(root.left);
+    System.out.print(root.val + " ");
+    printInorder(root.right);
+  }
+
+  public static void main(String[] args) {
+    int[] inorder = new int[] {9,3,15,20,7};
+    int[] postorder = new int[] {9,15,7,20,3};
+
+    TreeNode root = buildTree(inorder, postorder);
+    System.out.println("Given inorder = [9,3,15,20,7] and postorder = [9,15,7,20,3], the inorder of the built tree is:");
+
+    printInorder(root);
+    System.out.println();
+
+  }
+}
+
+// Resources: https://www.programcreek.com/2013/01/construct-binary-tree-from-inorder-and-postorder-traversal
