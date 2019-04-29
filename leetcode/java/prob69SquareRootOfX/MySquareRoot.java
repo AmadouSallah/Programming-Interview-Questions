906d094391d6db21509f48db5c5d5181fce3b733
AmadouSallah <amadousallah13@gmail.com>
2019-04-28 19:41:17 -0700

    Added solution for MySquareRoot.java with O(log x) runtime and O(1) space complexities

--- leetcode/java/prob69SquareRootOfX/MySquareRoot.java
+++ leetcode/java/prob69SquareRootOfX/MySquareRoot.java
@@ -22,7 +22,7 @@ Explanation: The square root of 8 is 2.82842..., and since
 */
 
 public class MySquareRoot {
-  // O(square root of x) runtiime and O(1) space complexities
+  // O(square root of x) runtime and O(1) space complexities
   public static int mySqrt(int x) {
     if (x == 0 || x == 1) return x;
     long i = 2;
@@ -30,9 +30,29 @@ public class MySquareRoot {
     return (int) i-1;
   }
 
+  // O(log x) runtime and O(1) space complexities
+  public static int mySqrt2(int x) {
+    if (x == 0 || x == 1) return x;
+    long left = 0, right = x;
+    while (left <= right) {
+      long mid = left + (right - left) / 2;
+      long square = mid * mid;
+      if (square == x) return (int) mid;
+      else if (square < x) left = mid + 1;
+      else right = mid - 1;
+    }
+    return (int) left - 1; // for cases where the decimal part is truncated, no perfect square
+  }
+
   public static void main(String[] args) {
+    System.out.println("Solution with O(square root of x) runtime and O(1) space complexities");
     System.out.println("mySqrt(2) = " + mySqrt(2)); // 2
     System.out.println("mySqrt(8) = " + mySqrt(8)); // 2
     System.out.println("mySqrt(2147395600) = " + mySqrt(2147395600)); // 46340
+
+    System.out.println("\nSolution with O(log x) runtime and O(1) space complexities");
+    System.out.println("mySqrt2(2) = " + mySqrt2(2)); // 2
+    System.out.println("mySqrt2(8) = " + mySqrt2(8)); // 2
+    System.out.println("mySqrt2(2147395600) = " + mySqrt2(2147395600)); // 46340
   }
 }
