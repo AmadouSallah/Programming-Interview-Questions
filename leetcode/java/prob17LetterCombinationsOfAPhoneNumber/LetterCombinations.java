ee8c1d384ee5f997ff6236b31416326616e48faa
AmadouSallah <amadousallah13@gmail.com>
2019-04-30 19:48:22 -0700

    Updated solution for LetterCombinations.java

--- leetcode/java/prob17LetterCombinationsOfAPhoneNumber/LetterCombinations.java
+++ leetcode/java/prob17LetterCombinationsOfAPhoneNumber/LetterCombinations.java
@@ -1,5 +1,7 @@
 /*
-Letter Combinations of a Phone Number
+Leetcode Problem 17: Letter Combinations of a Phone Number
+
+https://leetcode.com/problems/letter-combinations-of-a-phone-number/description/
 
 Given a digit string, return all possible letter combinations that the number could represent.
 
@@ -15,74 +17,49 @@ import java.util.*;
 public class LetterCombinations {
 
   public static List<String> letterCombinations(String digits) {
-
-    List<String> result = new ArrayList<String>();
+    List<String> result = new ArrayList<>();
 
     if (digits == null || digits.length() == 0) {
       return result;
     }
 
-    int len = digits.length();
-    char[] charArray = new char[len];
-    Map<Character, String> hmap = new HashMap<Character, String>();
-    hmap.put('2', "abc");
-    hmap.put('3', "def");
-    hmap.put('4', "ghi");
-    hmap.put('5', "jkl");
-    hmap.put('6', "mno");
-    hmap.put('7', "pqrs");
-    hmap.put('8', "tuv");
-    hmap.put('9', "wxyz");
+    Map<Character, String> map = new HashMap<Character, String>();
+    map.put('2', "abc");
+    map.put('3', "def");
+    map.put('4', "ghi");
+    map.put('5', "jkl");
+    map.put('6', "mno");
+    map.put('7', "pqrs");
+    map.put('8', "tuv");
+    map.put('9', "wxyz");
 
-    helper(digits, result, charArray, hmap, 0);
+    traverse(digits, result, map, "", 0);
 
     return result;
   }
 
-  private static void helper(String digits, List<String> result, char[] charArray, Map<Character, String> hmap, int index) {
+  public static void traverse(String digits, List<String> result, Map<Character, String> map, String path, int level) {
 
-    if ( index == digits.length() ) {
-      result.add(new String(charArray));
+    if (path.length() == digits.length()) {
+      result.add(path);
       return;
     }
 
-    char currentDigit = digits.charAt(index);
-    String currentString = hmap.get(currentDigit);
-    int len = currentString.length();
+    String s = map.get(digits.charAt(level));
 
-    for (int i = 0; i < len; i++) {
-      charArray[index] = currentString.charAt(i);
-      helper(digits, result, charArray, hmap, index+1);
+    for (int i = 0; i < s.length(); i++) {
+      String nextPath = path + s.charAt(i);
+      traverse(digits, result, map, nextPath, level+1);
     }
   }
 
-  public static String display(List<String> arr) {
-    String s = "[";
-    int n = arr.size();
-    for (int i = 0; i < n; i++) {
-      s += arr.get(i);
-      if (i < n - 1)
-        s += ", ";
-    }
-    s += "]";
-    return s;
-  }
 
   public static void main(String[] args) {
-    System.out.print("letterCombinations(null) = ");
-    System.out.println(display(letterCombinations(null)));
-
-    System.out.print("letterCombinations(\"\") = ");
-    System.out.println(display(letterCombinations("")));
-
-    System.out.print("letterCombinations(\"2\") = ");
-    System.out.println(display(letterCombinations("2")));
-
-    System.out.print("letterCombinations(\"23\") = ");
-    System.out.println(display(letterCombinations("23")));
-
-    System.out.print("letterCombinations(\"239\") = ");
-    System.out.println(display(letterCombinations("239")));
+    System.out.println("letterCombinations(null) = " + letterCombinations(null));
+    System.out.println("letterCombinations(\"\") = " + letterCombinations(""));
+    System.out.println("letterCombinations(\"2\") = " + letterCombinations("2"));
+    System.out.println("letterCombinations(\"23\") = " + letterCombinations("23") );
+    System.out.println("letterCombinations(\"239\")  " + letterCombinations("239"));
   }
 
 }
