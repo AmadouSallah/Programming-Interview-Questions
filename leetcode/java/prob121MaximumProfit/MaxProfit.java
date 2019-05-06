72828549e80cb322d1a2adf1a8b0ae176ca020ff
AmadouSallah <amadousallah13@gmail.com>
2019-05-05 18:09:14 -0700

    Added brute force solution and updated linear solution for MaxProfit.java

--- leetcode/java/prob121MaximumProfit/MaxProfit.java
+++ leetcode/java/prob121MaximumProfit/MaxProfit.java
@@ -19,18 +19,41 @@ In this case, no transaction is done, i.e. max profit = 0.
 */
 
 public class MaxProfit {
+  // Brute Force: O(n^2) runtime and O(1) space complexities
   public static int maxProfit(int[] prices) {
-    int n = prices.length, minPrice = Integer.MAX_VALUE, maxProfit = 0;
+    if (prices == null || prices.length == 0) return 0;
+    int maximumProfit = 0, n = prices.length;
+    for (int i = 0; i < n-1; i++) {
+      for (int j = i+1; j < n; j++) {
+        int currentProfit = prices[j] - prices[i];
+        if (currentProfit > maximumProfit) {
+          maximumProfit = currentProfit;
+        }
+      }
+    }
+    return maximumProfit;
+  }
+
+  // One Pass solution: O(n) runtime and O(1) space complexities
+  public static int maxProfit2(int[] prices) {
+    if (prices == null || prices.length == 0) return 0;
+    int n = prices.length, maxProfit = 0, minPrice = Integer.MAX_VALUE;
     for (int i = 0; i < n; i++) {
-      int elt = prices[i];
-      minPrice = Math.min(minPrice, elt);
-      maxProfit = Math.max(maxProfit, elt-minPrice);
+      if (prices[i] < minPrice)
+        minPrice = prices[i];
+      else if ( (prices[i] - minPrice) > maxProfit )
+        maxProfit = prices[i] - minPrice;
     }
     return maxProfit;
   }
 
   public static void main(String[] args) {
+    System.out.println("Using Brute Force solution: O(n^2) runtime and O(1) space complexities");
     System.out.println("maxProfit(new int[] {7,1,5,3,6,4) = " + maxProfit(new int[] {7,1,5,3,6,4}));
     System.out.println("maxProfit(new int[] {7, 6, 4, 3, 1) = " + maxProfit(new int[] {7, 6, 4, 3, 1}));
+
+    System.out.println("\nUsing linear solution: O(n) runtime and O(1) space complexities");
+    System.out.println("maxProfit2(new int[] {7,1,5,3,6,4) = " + maxProfit2(new int[] {7,1,5,3,6,4}));
+    System.out.println("maxProfit2(new int[] {7, 6, 4, 3, 1) = " + maxProfit2(new int[] {7, 6, 4, 3, 1}));
   }
 }
