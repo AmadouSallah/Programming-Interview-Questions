05bd63b49b15a36bf3f9a15cc87e016a796ac165
AmadouSallah <amadousallah13@gmail.com>
2019-07-06 01:32:41 -0700

    Added solution for AddTwoNumbersII.java

--- leetcode/java/prob445AddTwoNumbersII/AddTwoNumbersII.java
+++ leetcode/java/prob445AddTwoNumbersII/AddTwoNumbersII.java
@@ -15,3 +15,46 @@ Example:
 Input: (7 -> 2 -> 4 -> 3) + (5 -> 6 -> 4)
 Output: 7 -> 8 -> 0 -> 7
 */
+
+import java.util.Stack;
+
+public class AddTwoNumbersII {
+
+  public static class ListNode {
+    int val;
+    ListNode next;
+    public ListNode(int val) {
+      this.val = val;
+    }
+  }
+
+  public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+    Stack<Integer> s1 = new Stack<>(), s2 = new Stack<>();
+
+    while (l1 != null) {
+      s1.push (l1.val);
+      l1 = l1.next;
+    }
+
+    while (l2 != null) {
+      s2.push (l2.val);
+      l2 = l2.next;
+    }
+
+    ListNode head = null;
+    int carry = 0, sum;
+
+    while (carry != 0 || !s1.isEmpty() || !s2.isEmpty()) {
+      sum = carry; // reset sum to carry
+      if (!s1.isEmpty()) sum += s1.pop();
+      if (!s2.isEmpty()) sum += s2.pop();
+
+      carry = sum / 10;
+      ListNode node = new ListNode(sum % 10);
+      node.next = head;
+      head = node;
+    }
+    return head;
+  }
+}
