61cdb261bd10bebcbf8ea58b521dc74735f9e14e
AmadouSallah <amadousallah13@gmail.com>
2019-07-07 00:46:59 -0700

    Added solution for ReverseStringII.java

--- leetcode/java/prob541ReverseStringII/ReverseStringII.java
+++ leetcode/java/prob541ReverseStringII/ReverseStringII.java
@@ -14,4 +14,37 @@ Output: "bacdfeg"
 Restrictions:
 The string consists of lower English letters only.
 Length of the given string and k will in the range [1, 10000]
+
 */
+
+/*
+   0            k           2k          3k
+   |-----------|-----------|-----------|---
+   +--reverse--+           +--reverse--+
+*/
+
+public class ReverseStringII {
+  public static String reverseString(String s, int k) {
+    char[] arr = s.toCharArray();
+    int n = arr.length;
+    for (int i = 0; i < n; i += 2*k) {
+      int left = i, right = Math.min(i+k-1, n-1);
+      while (left < right) {
+        swap(arr, left, right);
+        left++;
+        right--;
+      }
+    }
+    return new String(arr);
+  }
+
+  public static void swap(char[] arr, int i, int j) {
+    char temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+
+  public static void main(String[] args) {
+    System.out.println("reverseString(\"abcdefg\", 2) = "+ reverseString("abcdefg", 2));
+  }
+}
