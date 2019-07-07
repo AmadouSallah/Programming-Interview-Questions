88005febc45fbaac118e367b5856c50180bde5a1
AmadouSallah <amadousallah13@gmail.com>
2019-07-07 00:13:56 -0700

    Added solution for ReverseWordsIII.java

--- leetcode/java/prob557ReverseWordsIII/ReverseWordsIII.java
+++ leetcode/java/prob557ReverseWordsIII/ReverseWordsIII.java
@@ -11,3 +11,28 @@ Output: "s'teL ekat edoCteeL tsetnoc"
 Note: In the string, each word is separated by single space and there will not be
 any extra space in the string.
 */
+
+public class ReverseWordsIII {
+
+  public static String reverseWords(String s) {
+    StringBuilder result = new StringBuilder();
+    StringBuilder word = new StringBuilder();
+    int n = s.length();
+    for (int i = 0; i < n; i++) {
+      char c = s.charAt(i);
+      if (c != ' ') {
+        word.append(c);
+      } else { // end of a word, so reverse it then add it to result
+        result.append(word.reverse());
+        result.append(' ');
+        word.setLength(0); // reset word to empty
+      }
+    }
+    result.append(word.reverse()); // add the last word reversed
+    return result.toString();
+  }
+
+  public static void main(String[] args) {
+    System.out.println(reverseWords("Let's take LeetCode contest"));
+  }
+}
