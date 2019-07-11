7feab657cb6519fcb56510fa1de51295ccecbd47
AmadouSallah <amadousallah13@gmail.com>
2019-07-11 03:45:41 -0700

    Added problem statement for FactorialTrailingZeroes.java

---
+++ leetcode/java/prob172FactorialTrailingZeroes/FactorialTrailingZeroes.java
@@ -0,0 +1,17 @@
+/*
+Problem 172. Factorial Trailing Zeroes
+https://leetcode.com/problems/factorial-trailing-zeroes/description/
+
+Given an integer n, return the number of trailing zeroes in n!.
+
+Example 1:
+
+Input: 3
+Output: 0
+Explanation: 3! = 6, no trailing zero.
+Example 2:
+
+Input: 5
+Output: 1
+Explanation: 5! = 120, one trailing zero.
+*/
