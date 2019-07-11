8317a589e818c4cbd096164a0131d73e0e7bef05
AmadouSallah <amadousallah13@gmail.com>
2019-07-11 03:49:39 -0700

    Added solution for FactorialTrailingZeroes.java

--- leetcode/java/prob172FactorialTrailingZeroes/FactorialTrailingZeroes.java
+++ leetcode/java/prob172FactorialTrailingZeroes/FactorialTrailingZeroes.java
@@ -15,3 +15,22 @@ Input: 5
 Output: 1
 Explanation: 5! = 120, one trailing zero.
 */
+
+public class FactorialTrailingZeroes {
+  public static int trailingZeroes(int n) {
+    long count = 0, i = 5;
+    while (n / i > 0) {
+      count += n / i;
+      i *= 5;
+    }
+    return (int) count;
+  }
+
+  public static void main(String[] args) {
+    System.out.println("trailingZeroes(3) = " + trailingZeroes(3));
+    System.out.println("trailingZeroes(5) = " + trailingZeroes(5));
+    System.out.println("trailingZeroes(25) = " + trailingZeroes(26));
+    System.out.println("trailingZeroes(125) = " + trailingZeroes(125));
+    System.out.println("trailingZeroes(1808548329) = " + trailingZeroes(1808548329));
+  }
+}
