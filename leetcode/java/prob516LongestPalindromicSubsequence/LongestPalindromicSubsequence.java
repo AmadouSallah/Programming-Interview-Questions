c6b54245617ccdc0a311e0873a2e39290d73ce08
AmadouSallah <amadousallah13@gmail.com>
2019-08-15 00:26:49 -0700

    Added problem statement for LongestPalindromicSubsequence.java

---
+++ leetcode/java/prob516LongestPalindromicSubsequence/LongestPalindromicSubsequence.java
@@ -0,0 +1,17 @@
+/*
+Leetcode Problem 516. Longest Palindromic Subsequence
+https://leetcode.com/problems/longest-palindromic-subsequence/description/
+
+Given a string s, find the longest palindromic subsequence's length in s.
+You may assume that the maximum length of s is 1000.
+
+Example 1:
+Input: "bbbab"
+Output: 4
+One possible longest palindromic subsequence is "bbbb".
+
+Example 2:
+Input: "cbbd"
+Output: 2
+One possible longest palindromic subsequence is "bb".
+*/
